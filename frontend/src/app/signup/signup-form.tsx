1fd8adb459262d79c6f81cb5db74f16c66597c70
Vincent <vafdaf11@gmail.com>
2024-05-24 20:58:55 +0200

    feat: Add name field to signup page

--- frontend/src/app/signup/signup-form.tsx
+++ frontend/src/app/signup/signup-form.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,6 +15,9 @@ import { z } from "zod";
 
 const signupSchema = z
   .object({
+    firstName: z.string().min(1, "Required"),
+    lastName: z.string().min(1, "Required"),
+    dateOfBirth: z.date(),
     email: z.string().min(1, "Required").email("Please enter a valid email"),
     password: z
       .string()
@@ -36,6 +38,27 @@ const signupSchema = z
   });
 
 type SignupData = z.infer<typeof signupSchema>;
+
+function TextField({ name, label }: { name: keyof SignupData; label: string }) {
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="block">{label}</FormLabel>
+          <FormControl>
+            <input
+              placeholder={label}
+              {...field}
+              className="w-full py-2 px-3 border-[1px] border-gray-200 rounded-md"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
 
 export default function SignupForm() {
   const form = useForm<SignupData>({
@@ -58,56 +81,11 @@ export default function SignupForm() {
         className="bg-white rounded-xl mx-auto my-10 w-1/2 p-5 shadow-lg gap-2 space-y-3"
       >
         <h1 className="text-lg font-medium mb-3">Create an Account</h1>
-        <FormField
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="block">Email</FormLabel>
-              <FormControl>
-                <input
-                  placeholder="Email"
-                  {...field}
-                  className="w-full py-2 px-3 border-[1px] border-gray-200 rounded-md"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  {...field}
-                  className="w-full py-2 px-3 border-[1px] border-gray-200 rounded-md"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="passwordConfirm"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <input
-                  type="password"
-                  placeholder="Confirm Password"
-                  {...field}
-                  className="w-full py-2 px-3 border-[1px] border-gray-200 rounded-md"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <TextField name="firstName" label="First Name" />
+        <TextField name="lastName" label="Last Name" />
+        <TextField name="email" label="Email" />
+        <TextField name="password" label="Password" />
+        <TextField name="passwordConfirm" label="Confirm Password" />
 
         <Button type="submit">Create</Button>
       </form>

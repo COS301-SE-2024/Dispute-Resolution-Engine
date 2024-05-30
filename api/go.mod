8073bc20dd057e6e7aa0d5c3d51e2c43d77c0d59
Caelan Hill <caelan.hill@gmail.com>
2024-05-30 14:56:50 +0200

    refactored code and fixed listener

--- api/go.mod
+++ api/go.mod
@@ -7,7 +7,4 @@ require (
 	github.com/lib/pq v1.10.9 // direct
 )
 
-require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/rs/cors v1.11.0 // indirect
-)
+require github.com/gorilla/mux v1.8.1
--- api/main.go
+++ api/main.go
@@ -1,69 +1,10 @@
 package main
 
 import (
-	// "fmt"
-	// "log"
-	// "net/http"
-
-	"api/database"
-	"encoding/json"
-	"log"
-	"net/http"
-
-	// "api/handlers"
-
-	"github.com/gorilla/mux"
-	// "github.com/rs/cors"
+	"api/api"
 )
 
-func writeJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
-
-
-type apiFunc func(w http.ResponseWriter, r *http.Request) error
-
-type APIError struct {
-	Error string
-}
-
-
-func makeHTTPHandler(fn apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := fn(w, r); err != nil {
-			writeJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
-		}
-	}
-}
-
-type APIServer struct {
-	listenAddr string
-}
-
-func newServer(liseningADD string) *APIServer {
-	return &APIServer{
-		listenAddr: liseningADD,
-	}
-}
-
-func (s *APIServer) Run() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/api", makeHTTPHandler(s.handleRequests))
-	log.Println("Server is running on port ", s.listenAddr)
-	http.ListenAndServe(s.listenAddr, router)
-
-}
-
-
-func (s *APIServer) handleRequests(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-
-
-
 func main() {
-    db.ConnectDB()
+	server := api.NewServer(":8080")
+	server.Run()
 }

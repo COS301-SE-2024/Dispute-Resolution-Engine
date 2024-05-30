94793de57f1e2f58b212bf73515b4d5880f351f6
Caelan Hill <caelan.hill@gmail.com>
2024-05-30 14:37:13 +0200

    setup basic request listener

--- api/go.mod
+++ api/go.mod
@@ -3,7 +3,11 @@ module api
 go 1.22.3
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/joho/godotenv v1.5.1 // direct
+	github.com/lib/pq v1.10.9 // direct
+)
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/rs/cors v1.11.0 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -1,33 +1,69 @@
 package main
 
 import (
-    // "fmt"
-    // "log"
-    // "net/http"
+	// "fmt"
+	// "log"
+	// "net/http"
 
-    "api/database"
-    // "api/handlers"
+	"api/database"
+	"encoding/json"
+	"log"
+	"net/http"
 
-    // "github.com/gorilla/mux"
-    // "github.com/rs/cors"
+	// "api/handlers"
+
+	"github.com/gorilla/mux"
+	// "github.com/rs/cors"
 )
 
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+
+func makeHTTPHandler(fn apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			writeJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func newServer(liseningADD string) *APIServer {
+	return &APIServer{
+		listenAddr: liseningADD,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api", makeHTTPHandler(s.handleRequests))
+	log.Println("Server is running on port ", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+
+}
+
+
+func (s *APIServer) handleRequests(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+
+
 func main() {
-    // Connect to the database
     db.ConnectDB()
-
-    // Create a new router
-    // router := mux.NewRouter()
-
-    // Define the routes
-    // router.HandleFunc("/users", handlers.GetUsers).Methods("GET")
-
-    // // Handle CORS
-    // c := cors.New(cors.Options{
-    //     AllowedOrigins: []string{"http://localhost:3000"},
-    // })
-
-    // // Start the server
-    // fmt.Println("Starting server on :8080")
-    // log.Fatal(http.ListenAndServe(":8080", c.Handler(router)))
 }

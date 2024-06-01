b27a448a7a8fc152e0064423c5220e248b0f6197
Vincent <vafdaf11@gmail.com>
2024-06-01 22:59:56 +0200

    feat: Integrate signup/login

--- frontend/src/app/lib/auth.ts
+++ frontend/src/app/lib/auth.ts
@@ -3,20 +3,40 @@
 import { LoginData } from "../login/login-form";
 import { SignupData } from "../signup/signup-form";
 
+const API_URL = process.env.API_URL;
+
 export async function signup(data: SignupData): Promise<string> {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      console.log(data);
-      resolve("Something happened");
-    }, 2000)
-  );
+  const response = await fetch(`${API_URL}/api`, {
+    method: "POST",
+    body: JSON.stringify({
+      request_type: "create_account",
+      body: {
+        first_name: data.firstName,
+        surname: data.lastName,
+        email: data.email,
+        password_hash: data.password
+      }
+    })
+  }).then(res => res.json());
+  if(typeof response == "string") {
+    return response;
+  }
+  return response.Error;
 }
 
 export async function login(data: LoginData): Promise<string> {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      console.log(data);
-      resolve("Logged in!!");
-    }, 2000)
-  );
+  const response = await fetch(`${API_URL}/api`, {
+    method: "POST",
+    body: JSON.stringify({
+      request_type: "login",
+      body: {
+        email: data.email,
+        password_hash: data.password,
+      }
+    })
+  }).then(res => res.json());
+  if(typeof response == "string") {
+    return response;
+  }
+  return response.Error;
 }

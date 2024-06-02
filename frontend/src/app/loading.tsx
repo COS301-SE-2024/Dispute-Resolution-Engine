91e0525585c9d89d975ff686782897af434528ed
S3BzA <81089003+S3BzA@users.noreply.github.com>
2024-06-02 11:25:53 +0200

    docs add loader page

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
-import Ralph from '../../public/ralph.png';
 
-export default function Loader() {
+export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
 	  <div className="animate-spin bg-white rounded-full h-32 w-32 border-8 border-black overflow-hidden">
-		<Image src={Ralph} alt="Ralph" className="h-32 w-32 mt-6" />
+		<Image src="/ralph.png" width={1362} height={1362} alt="Ralph" className="h-32 w-32 mt-6" />
 	  </div>
 	</div>
   );

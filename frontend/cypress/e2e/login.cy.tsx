ca25cb01d3627a84b0d5a422e40f62054b56674e
michael <michael@horwitz.co.za>
2024-06-02 16:29:58 +0200

    feat changed returned message for login

--- frontend/cypress/e2e/login.cy.tsx
+++ frontend/cypress/e2e/login.cy.tsx
@@ -1,12 +1,8 @@
 describe('Navigation', () => {
 	it('should siupgn to the server', () => {
-		cy.visit('http://localhost:3000/signup')
-		cy.contains('First Name').type('Alice')
-		cy.contains('Last Name').type('Smith')
+		cy.visit('http://localhost:3000/login')
 		cy.contains('Email').type('alice@smith.co.za')
 		cy.contains('Password').type('Password1234#')
-		cy.contains('Confirm Password').type('Password1234#')
-		cy.get('button').contains('Create').click()
 		cy.get('p').contains('Something happened')
 	})	
   })

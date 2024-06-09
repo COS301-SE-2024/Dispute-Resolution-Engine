5d49b1133f4ebbc5001a1f1a0041fbbc2fedbbd2
ZaguePrime <kivashin47@gmail.com>
2024-06-09 17:06:31 +0200

    feat/api-rework: added check for duplicate emails

--- api/handlers/userHandler.go
+++ api/handlers/userHandler.go
@@ -3,9 +3,14 @@ package handlers
 import (
 	"api/models"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
+
+type StringWrapper struct {
+	Data string `json:"Data"`
+}
 
 func (h handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -18,13 +23,37 @@ func (h handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	err = json.Unmarshal(body, &user)
 
-	if result := h.DB.Create(&user); result.Error != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+	duplicate := h.checkUserExists(user.Email)
+
+	if duplicate {
+		errJson := StringWrapper{"User already exists"}
+		jsonData, err := json.Marshal(errJson)
+		if err != nil {
+			fmt.Println("Error marshalling JSON", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusConflict)
+		w.Write(jsonData)
 		return
 	}
+	errJson, err := json.Marshal(StringWrapper{"Failed to create user"})
+	if result := h.DB.Create(&user); result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusConflict)
+		w.Write(errJson)
+		return
+	}
+	resbody, err := json.Marshal(StringWrapper{"Account created successfully"})
 
 	// Send a 201 created response
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode("Account created successfully")
+	w.Write(resbody)
+}
+
+func (h handler) checkUserExists(email string) bool {
+	var user models.User
+	h.DB.Where("email = ?", email).First(&user)
+	return user.Email != ""
 }

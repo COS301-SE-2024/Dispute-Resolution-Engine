d1d567053e7f65fd04c47c8c7b37b51461729209
ZaguePrime <kivashin47@gmail.com>
2024-06-09 18:15:47 +0200

    feat/api-rework: added getTime function

--- api/handlers/Auth.go
+++ api/handlers/Auth.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"api/models"
+	"api/utilities"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -13,6 +14,7 @@ type StringWrapper struct {
 }
 
 func (h handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	hasher := utilities.NewArgon2idHash(1,12288,4,32,16)
 	defer r.Body.Close()
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -37,6 +39,15 @@ func (h handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		w.Write(jsonData)
 		return
 	}
+
+	hashAndSalt := hasher.HashPassword(user.PasswordHash)
+
+	user.CreatedAt = utilities.GetCurrentTime()
+	user.UpdatedAt = utilities.GetCurrentTime()
+	user.LastLogin = nil
+	user.PasswordHash = string(hashAndSalt.Hash)
+	user.Salt = string(hashAndSalt.Salt)
+
 	errJson, err := json.Marshal(StringWrapper{"Failed to create user"})
 	if result := h.DB.Create(&user); result.Error != nil {
 		w.Header().Set("Content-Type", "application/json")
@@ -49,6 +60,52 @@ func (h handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	// Send a 201 created response
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	w.Write(resbody)
+}
+
+func (h handler) Login (w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
+
+	var user models.User
+	err = json.Unmarshal(body, &user)
+
+	var dbUser models.User
+	h.DB.Where("email = ?", user.Email).First(&dbUser)
+
+	if dbUser.Email == "" {
+		errJson, err := json.Marshal(StringWrapper{"User not found"})
+		if err != nil {
+			fmt.Println("Error marshalling JSON", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(errJson)
+		return
+	}
+
+	if dbUser.PasswordHash != user.PasswordHash {
+		errJson, err := json.Marshal(StringWrapper{"Invalid password"})
+		if err != nil {
+			fmt.Println("Error marshalling JSON", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write(errJson)
+		return
+	}
+
+	resbody, err := json.Marshal(StringWrapper{"Login successful"})
+
+	// Send a 200 OK response
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write(resbody)
 }
 

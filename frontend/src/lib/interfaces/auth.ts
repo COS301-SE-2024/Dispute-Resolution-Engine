057687013aa675f5f8e70adf10410e88b500e1ea
Vincent <vafdaf11@gmail.com>
2024-06-17 20:28:46 +0200

    feat: Add password resetting types

--- frontend/src/lib/interfaces/auth.ts
+++ frontend/src/lib/interfaces/auth.ts
@@ -62,3 +62,29 @@ export interface VerifyRequest {
  * Returns the JWT of the verified user
  */
 export type VerifyResponse = string;
+
+/**
+ * Request used to reset the password of an authenticated user.
+ *
+ * Add a HTTP header with authentication header:
+ * ```http
+ * Authorization: Bearer <JWT>
+ * ```
+ */
+export interface ResetPasswordRequest {
+  oldPassword: string;
+  newPassword: string;
+}
+/**
+ * Returns a new updated JWT for the password reset
+ */
+export type ResetPasswordResponse = string;
+
+export interface ForgotPasswordRequest {
+  email: string;
+}
+
+/**
+ * Returns a success message
+ */
+export type ForgotPasswordResponse = string;

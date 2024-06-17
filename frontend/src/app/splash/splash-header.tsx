5e3ef18a1a0be3f69769c4ce9038e5492b640b8a
michael <michael@horwitz.co.za>
2024-06-17 19:33:56 +0200

    Updated links on splash page

--- frontend/src/app/splash/splash-header.tsx
+++ frontend/src/app/splash/splash-header.tsx
@@ -1,37 +1,32 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function SplashHeader() {
-  const buttonClass : string = "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+  const buttonClass: string =
+    "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50";
   return (
     <div className="container mx-auto px-4 md:px-6 lg:px-8 bg-black z-40">
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-        <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
+        <Link href="/splash" className="mr-6 hidden lg:flex" prefetch={false}>
           <RalphIcon className="h-6 w-6" />
           <span className="sr-only">Car E-commerce</span>
         </Link>
         <div className="ml-auto flex gap-2">
-          <Link
-            href="#"
-            className={buttonClass}
-            prefetch={false}
-          >
+          <Link href="/splash" className={buttonClass} prefetch={false}>
             Home
           </Link>
-          <Link
-            href="#"
-            className={buttonClass}
-            prefetch={false}
-          >
+          <Link href="/disputes" className={buttonClass} prefetch={false}>
             Disputes
           </Link>
-          <Button variant="outline" className="justify-self-end px-2 py-1 text-xs">
-            Login
-          </Button>
-          <Button className="justify-self-end px-2 py-1 text-xs">
-            Sign Up
-          </Button>
+          <Link href="/login" prefetch={false}>
+            <Button variant="outline" className="justify-self-end px-2 py-1 text-xs">
+              Login
+            </Button>
+          </Link>
+          <Link href="/signup" prefetch={false}>
+            <Button className="justify-self-end px-2 py-1 text-xs">Sign Up</Button>
+          </Link>
         </div>
       </header>
     </div>
@@ -39,14 +34,9 @@ export default function SplashHeader() {
 }
 
 function RalphIcon(props: any) {
-  const imgSize : number = 55
+  const imgSize: number = 55;
   return (
-    <Image
-      src="/ralph.png"
-      alt="Racoon Logo"
-      width={imgSize}
-      height={imgSize}
-    />
+    <Image src="/ralph.png" alt="Racoon Logo" width={imgSize} height={imgSize} />
     // <svg
     //   {...props}
     //   xmlns="http://www.w3.org/2000/svg"
@@ -64,5 +54,5 @@ function RalphIcon(props: any) {
     //   <path d="M9 17h6" />
     //   <circle cx="17" cy="17" r="2" />
     // </svg>
-  )
+  );
 }

ea6f4b1e885f30a3b5c66096ec8d2fc8d09dfcad
Vincent <vafdaf11@gmail.com>
2024-06-21 14:59:47 +0200

    feat: Integrate UI with API stubs

--- frontend/src/app/archive/search/page.tsx
+++ frontend/src/app/archive/search/page.tsx
@@ -1,25 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { searchArchive } from "@/lib/api/archive";
+import { ArchivedDisputeSummary } from "@/lib/interfaces/archive";
 import { ExternalLink } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-function SearchResult() {
+function SearchResult(props: ArchivedDisputeSummary) {
   return (
     <Card className="border-none">
       <CardHeader>
         <Link href="/archive/id">
-          <CardTitle>Dispute Title</CardTitle>
+          <CardTitle>{props.title}</CardTitle>
         </Link>
       </CardHeader>
       <CardContent asChild className="dark:text-white/50">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quo eveniet, veniam
-          facere voluptatem est...
-        </p>
+        <p>{props.summary}</p>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <p>Status: Resolved</p>
+        <p>{props.resolution}</p>
         <Button>
           <ExternalLink size="1rem" className="mr-2" />
           Read More
@@ -29,7 +29,28 @@ function SearchResult() {
   );
 }
 
-export default function ArchiveSearch() {
+interface SearchParams {
+  q?: string;
+}
+
+export default async function ArchiveSearch({ searchParams }: { searchParams: SearchParams }) {
+  if (!searchParams.q) {
+    redirect("/archive");
+  }
+
+  const { data, error } = await searchArchive({
+    search: searchParams.q,
+  });
+
+  if (error) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full gap-5 w-2/3 mx-auto">
+        <h1 className="text-4xl font-bold tracking-wide">Oops, something went wrong :(</h1>
+        <p className="dark:text-white/50">{error}</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <header className="p-3 items-start gap-2 flex flex-col">
@@ -44,8 +65,8 @@ export default function ArchiveSearch() {
         </div>
       </header>
       <main className="space-y-3 mx-8">
-        {[...Array(10).keys()].map((i) => (
-          <SearchResult key={i} />
+        {data!.map((dispute) => (
+          <SearchResult key={dispute.id} {...dispute} />
         ))}
       </main>
     </>

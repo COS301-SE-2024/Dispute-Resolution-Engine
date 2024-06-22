02adc2056fd31bebea09607e017d7012f6b12e33
michael <michael@horwitz.co.za>
2024-06-22 18:36:01 +0200

    feat styled create dispute

--- frontend/src/app/disputes/create/page.tsx
+++ frontend/src/app/disputes/create/page.tsx
@@ -7,24 +7,25 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
+  FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 const formSchema = z.object({
   title: z.string().min(2).max(50),
   respondentEmail: z.string().email(),
   respondentTelephone: z.string().min(10).max(15),
   summary: z.string().min(3).max(500),
-  file: z.instanceof(FileList).optional(),
+  file: z.instanceof(FileList).optional()
 });
 
 export default function CreateDispute() {
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+    resolver: zodResolver(formSchema)
   });
 
   const fileRef = form.register("file");
@@ -34,73 +35,78 @@ export default function CreateDispute() {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full p-10">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="We be beefing" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="respondentEmail"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>RespondentEmail</FormLabel>
-              <FormControl>
-                <Input placeholder="abc@example.com" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="respondentTelephone"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Respondent Telephone</FormLabel>
-              <FormControl>
-                <Input placeholder="Mr Biggest Op" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="summary"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Summary</FormLabel>
-              <FormControl>
-                <Input placeholder="He stole my chib" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="file"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>File</FormLabel>
-                <FormControl>
-                  <Input type="file" placeholder="shadcn" {...fileRef} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            );
-          }}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <Card className="w-full">
+      <CardHeader><CardTitle className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-2xl">Create a
+        Dispute</CardTitle></CardHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full pt-0 p-10">
+          <div className="space-y-5">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="We be beefing" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="respondentEmail"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>RespondentEmail</FormLabel>
+                  <FormControl>
+                    <Input placeholder="abc@example.com" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="respondentTelephone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Respondent Telephone</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Mr Biggest Op" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="summary"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Summary</FormLabel>
+                  <FormControl>
+                    <Input placeholder="He stole my chib" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>File</FormLabel>
+                    <FormControl>
+                      <Input type="file" placeholder="shadcn" {...fileRef} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </Form></Card>
   );
 }

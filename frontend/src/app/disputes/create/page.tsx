36c306e45c6e5477f3e53ebbd3f1bbc364b93b9e
michael <michael@horwitz.co.za>
2024-06-22 18:16:15 +0200

    feat added file proper

--- frontend/src/app/disputes/create/page.tsx
+++ frontend/src/app/disputes/create/page.tsx
@@ -1,73 +1,41 @@
 "use client";
 
-import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { zodResolver } from "@hookform/resolvers/zod";
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { API_URL } from "@/lib/utils";
-import { Evidence } from "@/lib/interfaces/dispute";
-export interface DisputeCreateRequest {
-  description: string;
-  desired_outcome: string;
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-  respondent: {
-    full_name: string;
-    email: string;
-    telephone: string;
-  };
-
-  jurisdictional_basis: Evidence;
-
-  /**
-   * IDs of all adjudicators to be appointed
-   */
-  adjudicators: string[];
-
-  // This should be FormData, but I don't know how to annotate that
-  evidence: Evidence[];
-}
-// Add field for evidence which is a file upload
 const formSchema = z.object({
   title: z.string().min(2).max(50),
   respondentEmail: z.string().email(),
   respondentTelephone: z.string().min(10).max(15),
-  summary: z.string().max(500),
-})
+  summary: z.string().min(3).max(500),
+  file: z.instanceof(FileList).optional(),
+});
 
 export default function CreateDispute() {
   const form = useForm<z.infer<typeof formSchema>>({
-    defaultValues: {
-      title: "",
-      respondentEmail: "",
-      respondentTelephone: "",
-      summary: "",
-    },
     resolver: zodResolver(formSchema),
-  })
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    const data = fetch(`${API_URL}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    })
-    console.log(values)
-  }
+  });
+
+  const fileRef = form.register("file");
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full p-10">
         <FormField
           control={form.control}
           name="title"
@@ -116,8 +84,23 @@ export default function CreateDispute() {
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>File</FormLabel>
+                <FormControl>
+                  <Input type="file" placeholder="shadcn" {...fileRef} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }

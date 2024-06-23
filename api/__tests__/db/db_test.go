c57c2b801922d6d80827510df6b31e998ed0a878
michael <michael@horwitz.co.za>
2024-06-23 13:10:42 +0200

    added db tests

---
+++ api/__tests__/db/db_test.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+    "testing"
+	// "api/db"
+    //"github.com/stretchr/testify/assert"
+)
+
+type MockStorage struct{}
+
+// func TestNewServer(t *testing.T) {
+//     store, err := storage.NewPostgresStore()
+// 	server := api.NewServer(":8080", store)
+// 	server.Run()	
+// }
+func TestRandomSales(t *testing.T) {
+
+}

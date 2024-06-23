1b52b5e6841fc02ec92e9674cbde2f276c20a7c0
Vincent <vafdaf11@gmail.com>
2024-06-23 16:42:15 +0200

    feat: Add address fields

--- frontend/src/components/form/address-fields.tsx
+++ frontend/src/components/form/address-fields.tsx
@@ -1,31 +1,39 @@
+import { useId } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import CountrySelect from "./country-select";
 
 export default function AddressFields() {
+  const countryId = useId();
+  const provinceId = useId();
+  const cityId = useId();
+  const addr1Id = useId();
+  const addr2Id = useId();
+  const addr3Id = useId();
+
   return (
     <fieldset className="space-y-3">
       <div>
-        <Label htmlFor="addrCountry">Country</Label>
+        <Label htmlFor={countryId}>Country</Label>
         <CountrySelect name="addrCountry" />
       </div>
       <div>
-        <Label htmlFor="addrProvince">Province</Label>
+        <Label htmlFor={provinceId}>Province</Label>
         <Input
-          autoComplete="off"
-          id="addrProvince"
-          placeholder="Province"
-          type="text"
+          id={provinceId}
           name="addrProvince"
+          type="text"
+          placeholder="Province"
+          autoComplete="off"
         />
       </div>
       <div>
-        <Label htmlFor="addrCity">City</Label>
-        <Input id="addrCity" placeholder="City" type="text" name="addrCity" />
+        <Label htmlFor={cityId}>City</Label>
+        <Input id={cityId} placeholder="City" type="text" name="addrCity" />
       </div>
 
-      <div className="space-y-2">
-        <Label htmlFor="addrStreet2">Street Address</Label>
+      <div className="space-y-1">
+        <Label htmlFor={addr1Id}>Address Line 1</Label>
         <Input
           autoComplete="address-line1"
           id="addrStreet"
@@ -33,18 +41,23 @@ export default function AddressFields() {
           placeholder="Address Line 1"
           type="text"
         />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor={addr2Id}>Address Line 2</Label>
         <Input
-          autoComplete="address-line2"
-          id="addrStreet2"
+          id={addr2Id}
           name="addrStreet2"
+          autoComplete="address-line2"
           placeholder="Address Line 2"
           type="text"
         />
-
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor={addr3Id}>Address Line 3</Label>
         <Input
-          autoComplete="address-line3"
-          id="addrStreet3"
+          id={addr3Id}
           name="addrStreet3"
+          autoComplete="address-line3"
           placeholder="Address Line 3"
           type="text"
         />

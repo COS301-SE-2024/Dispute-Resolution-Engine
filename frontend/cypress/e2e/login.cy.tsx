2a51b2539c2ef56b5e8e2d6d7147c16a5128523d
michael <michael@horwitz.co.za>
2024-06-23 21:38:03 +0200

    feat updated login and signup e2e tests

--- frontend/cypress/e2e/login.cy.tsx
+++ frontend/cypress/e2e/login.cy.tsx
@@ -1,5 +1,5 @@
 describe('Navigation', () => {
-	it('should siupgn to the server', () => {
+	it('should login to the server', () => {
 		cy.visit('http://localhost:3000/login')
 		cy.contains('Email').type('alice@smith.co.za')
 		cy.contains('Password').type('Password1234#')

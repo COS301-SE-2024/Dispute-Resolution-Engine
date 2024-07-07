97907c366b798dcfe420e9a5d5ffb7a0a80f3fa0
ZaguePrime <kivashin47@gmail.com>
2024-07-07 20:30:30 +0200

    feat/notifications: changes to notification function

--- api/handlers/notifications.go
+++ api/handlers/notifications.go
@@ -8,25 +8,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupNotificationRoutes(g *gin.RouterGroup, h Notification) {
-	g.POST("/invite", h.InviteParty)
-}
+// func SetupNotificationRoutes(g *gin.RouterGroup, h Notification) {
+// 	g.POST("/invite", h.sendAdminNotification)
+// }
 
-func (h Notification) InviteParty(c *gin.Context) {
+func (h Notification) sendAdminNotification(c *gin.Context, resEmail string) {
+
+	//get claims
 	jwtClaims := middleware.GetClaims(c)
 	if jwtClaims == nil {
 		c.JSON(http.StatusUnauthorized, models.Response{Error: "Unauthorized"})
 		return
 	}
 
+	//parse the request body
 	var reqInv models.DisputeNotifyInvite
 	if err := c.BindJSON(&reqInv); err != nil {
 		c.JSON(http.StatusBadRequest, models.Response{Error: "Invalid request"})
 		return
 	}
 
+	//get the dispute details using ID from request body
 	var dbDispute models.Dispute
 	h.DB.Where("id = ?", reqInv.DisputeID).First(&dbDispute)
 
-
+	var respondentEmail = resEmail
+	//For now will stub it, but here we will finalize and send the appropriate email to invite a party
+	
 }

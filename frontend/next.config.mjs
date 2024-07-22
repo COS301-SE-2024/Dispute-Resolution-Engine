2215ecdd23c5a1a7f89f0a47f7df8c70dc1f7e7f
Vincent <vafdaf11@gmail.com>
2024-07-22 12:40:05 +0200

    feat: Redirect to splash page instead of signup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/",
-                destination: "/signup",
+                destination: "/splash",
                 permanent: true
             }
         ]

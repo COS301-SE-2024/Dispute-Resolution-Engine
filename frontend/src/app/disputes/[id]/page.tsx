7214f67f914ca203afd3ecbde630b309d90a229e
michael <michael@horwitz.co.za>
2024-07-23 09:39:49 +0200

    refac: moved dropdown client side

--- frontend/src/app/disputes/[id]/page.tsx
+++ frontend/src/app/disputes/[id]/page.tsx
@@ -1,86 +1,24 @@
-"use client"
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { getDisputeDetails, updateDisputeStatus } from "@/lib/api/dispute";
 import { Metadata } from "next";
 import { File, WorkflowIcon } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useState } from "react";
-// eslint-disable-next-line @next/next/no-document-import-in-page
-import { Head } from "next/document";
+import DisputeHeader from "@/app/disputes/[id]/Dropdown";
 type Props = {
   params: { id: string };
 };
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   return {
-//     title: `Dispute ${params.id}`,
-//     description: "Dispute description",
-//   };
-// }
-
-function DisputeHeader({
-  id,
-  label,
-  startDate,
-  status,
-}: {
-  id: string;
-  label: string;
-  startDate: string;
-  status: string;
-}) {
-  const [funcStatus, setStatus] = useState("")
-  const handleStatusChange = async (newStatus: string) => {
-    const result = await updateDisputeStatus(id, newStatus);
-    if (!result.error) {
-      setStatus(newStatus); // Update status state if API call is successful
-    } else {
-      console.error("Failed to update status:", result.error);
-    }
-  }
-  return (
-    <header className="p-4 py-6 flex">
-      <div className="grow">
-        <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-2xl">{label}</h1>
-        <span>Started: {startDate}</span>
-      </div>
-
-      <dl className="grid grid-cols-2 gap-2">
-        <dt className="text-right font-bold">Dispute ID: </dt>
-        <dd>{id}</dd>
-        <dt className="text-right font-bold">Status: </dt>
-        <dd>
-          <DropdownMenu >
-            <DropdownMenuTrigger asChild>
-              <Badge>{status}</Badge>
-              </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Next Steps</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Waiting for Admin Approval</DropdownMenuItem>
-              <DropdownMenuItem>Waiting for respondent</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </dd>
-      </dl>
-    </header>
-  );
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: `Dispute ${params.id}`,
+    description: "Dispute description",
+  };
 }
+
 
 export default async function DisputePage({ params }: Props) {
   const { data, error } = await getDisputeDetails(params.id);
-
   if (error || !data) {
     return <h1>{error}</h1>;
   }
@@ -95,10 +33,6 @@ export default async function DisputePage({ params }: Props) {
 
   return (
     <div className="grow overflow-y-auto flex flex-col">
-      <Head>
-        <title>{`Dispute ${params.id}`}</title>
-        <meta name="description" content="Dispute description" />
-      </Head>
       <DisputeHeader
         id={data.id}
         label={data.title}

ca8da70732971d3472a62097331fa1e4f44051cd
Vincent <vafdaf11@gmail.com>
2024-07-28 16:47:23 +0200

    feat: Add archive recommendations

--- frontend/src/app/archive/page.tsx
+++ frontend/src/app/archive/page.tsx
@@ -1,7 +1,10 @@
 import Loader from "@/components/Loader";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { fetchArchiveHighlights } from "@/lib/api/archive";
+import { ArchivedDisputeSummary } from "@/lib/interfaces/archive";
 import { ExternalLink } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -10,22 +13,21 @@ export const metadata: Metadata = {
   title: "DRE - Archive",
 };
 
-function ArchivedDispute() {
+function ArchivedDispute(props: ArchivedDisputeSummary) {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Dispute Title</CardTitle>
+        <CardTitle>{props.title}</CardTitle>
       </CardHeader>
       <CardContent asChild className="dark:text-white/50">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quo eveniet, veniam
-          facere voluptatem est...
-        </p>
+        <p>{props.summary}</p>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <p>Status: Resolved</p>
+        {props.category.map((cat, i) => (
+          <Badge key={i}>{cat}</Badge>
+        ))}
         <Button asChild>
-          <Link href="/archive/id">
+          <Link href={`/archive/${props.id}`}>
             <ExternalLink size="1rem" className="mr-2" />
             Read More
           </Link>
@@ -34,7 +36,12 @@ function ArchivedDispute() {
     </Card>
   );
 }
-export default function Archive() {
+export default async function Archive() {
+  const { data, error } = await fetchArchiveHighlights(3);
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-full gap-5 w-2/3 mx-auto">
       <header className="mx-auto w-fit text-center">
@@ -53,8 +60,8 @@ export default function Archive() {
       <footer>
         <h2 className="text-2xl font-semibold mb-4">Resolved Disputes</h2>
         <div className="flex flex-col md:grid md:grid-cols-3 gap-4">
-          {[...Array(3).keys()].map((i) => (
-            <ArchivedDispute key={i} />
+          {data!.archives.map((props, i) => (
+            <ArchivedDispute key={i} {...props} />
           ))}
         </div>
       </footer>

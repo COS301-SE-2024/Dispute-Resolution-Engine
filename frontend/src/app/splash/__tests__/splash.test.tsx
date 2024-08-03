7caf79c20ec3d254668e270043e38997b97ce870
Michael Horwitz <michael@horwitz.co.za>
2024-08-03 23:19:31 +0200

    feat: changed test to regex

--- frontend/src/app/splash/__tests__/splash.test.tsx
+++ frontend/src/app/splash/__tests__/splash.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 
 import "@testing-library/jest-dom";
 import SplashPage from "@/app/splash/splash";
@@ -7,6 +7,6 @@ import SplashPage from "@/app/splash/splash";
 describe('Splash Page', () => {
   it('renders without crashing', () => {
     render(<SplashPage />);
-    expect(screen.getByText('Here to streamline your ADR process')).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: /Here to streamline your ADR process./i })).toBeInTheDocument();
   });
 });

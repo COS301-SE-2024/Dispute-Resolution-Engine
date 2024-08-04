a6a61b30bc472c14dba9bddfcc00b526d70cbaed
Caelan Hill <caelan.hill@gmail.com>
2024-08-04 18:44:53 +0200

    feat/created first iteration of logging function

--- api/auditLogger/disputeProceedingsLogger.go
+++ api/auditLogger/disputeProceedingsLogger.go
@@ -3,6 +3,8 @@ package auditLogger
 import (
 	"api/env"
 	"api/models"
+	"api/utilities"
+	"encoding/json"
 
 	"gorm.io/gorm"
 )
@@ -17,6 +19,25 @@ func NewDisputeProceedingsLogger(db *gorm.DB) DisputeProceedingsLogger {
 	return DisputeProceedingsLogger{DB: db, EnvReader: env.NewEnvLoader()}
 }
 
-func (d DisputeProceedingsLogger) LogDisputeProceedings(proceedingType models.EventTypes, jsonMessage string) {
+func (d DisputeProceedingsLogger) LogDisputeProceedings(proceedingType models.EventTypes, jsonMessage string) error{
+	// Parse the JSON message
+	logger := utilities.NewLogger()
 
+	var eventData map[string]interface{}
+	err := json.Unmarshal([]byte(jsonMessage), &eventData)
+	if err != nil {
+		logger.WithError(err).Error("Error parsing JSON message")
+		return err
+	}
+
+	// Log the dispute proceedings
+	err = d.DB.Create(&models.EventLog{
+		EventType: proceedingType,
+		EventData: eventData,
+	}).Error
+	if err != nil {
+		logger.WithError(err).Error("Error logging dispute proceedings")
+		return err
+	}
+	return nil
 }

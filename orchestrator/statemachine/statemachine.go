42b25492c0c8eb986d7b512afea4f5dcab81903a
S3BzA <81089003+S3BzA@users.noreply.github.com>
2024-08-05 21:10:15 +0200

    chore: update timer to use channels

--- orchestrator/statemachine/statemachine.go
+++ orchestrator/statemachine/statemachine.go
@@ -20,34 +20,59 @@ type StateMachine struct {
 	stateMachine *stateless.StateMachine
 }
 
-func (s *StateMachine) Init(workflow workflow.IWorkflow) {
+func CountDownTimer(ctx context.Context, duration time.Duration) error {
+    timer := time.NewTimer(duration)
+    defer timer.Stop()
+
+    select {
+    case <-timer.C:
+        return nil // timer expired
+    case <-ctx.Done():
+        return ctx.Err() // context cancelled or timed out
+    }
+}
+
+func (s *StateMachine) Init(wf workflow.IWorkflow) {
 	logger := utilities.NewLogger().LogWithCaller()
 	logger.Info("Initialising state machine")
-	initState := workflow.GetInitialState() // this whole sequence is a bit weird, but idk how else to do it
+	initState := wf.GetInitialState() // this whole sequence is a bit weird, but idk how else to do it
 	initStatePtr := &initState              // without changing the workflow interface
 	s.stateMachine = stateless.NewStateMachine(initStatePtr.GetName())
 
 	// for every state in the workflow, add it to the state machine
-	for _, state := range workflow.GetStates() {
+	for _, state := range wf.GetStates() {
 		// for every related transition from the state, configure the state with the transition
-		toTransitions := workflow.GetTransitionsByFrom(state.GetName())
+		toTransitions := wf.GetTransitionsByFrom(state.GetName())
 		for _, transition := range toTransitions {
 			s.stateMachine.Configure(state.GetName()).
 			Permit(transition.GetTrigger(), transition.GetTo())
 		}
 		
 		// for every timer in the state, add it to the state machine
-
 		for _, timer := range state.GetTimers() {
-			s.stateMachine.Configure(state.GetName()).(func(ctx context.Context, args ...any) error {
-				if timer.HasDeadlinePassed() {
-					logger.WithField("state", state.GetName()).Warn("Timer has passed")
-				} else {
-					logger.WithField("state", state.GetName()).Info("Timer has not passed")
-				}			
-				return nil
-			})
+			s.stateMachine.Configure(state.GetName()).OnEntry(func(ctx context.Context, args ...any) error {
+				timerCtx, cancel := context.WithTimeout(ctx, timer.GetDuration())
+                defer cancel()
+
+                err := CountDownTimer(timerCtx, timer.GetDuration()) // this is where the timer is actually started
+                if err != nil {
+                    if err == context.DeadlineExceeded {
+                        logger.WithField("state", state.GetName()).Warn("Timer expired")
+                        // Handle timer expiration, e.g., transition to a timeout state
+                        s.stateMachine.Fire("timeoutEvent")
+                    } else {
+                        logger.WithField("state", state.GetName()).Warn("Context cancelled")
+                    }
+                }
+                return nil
+			})	
 		}
 	}
+	s.stateMachine.Configure(workflow).
+        OnEntry(func(ctx context.Context, args ...any) error {
+            logger.Warn("Entered timeout state")
+            // Handle timeout scenario
+            return nil
+        })
 }
 

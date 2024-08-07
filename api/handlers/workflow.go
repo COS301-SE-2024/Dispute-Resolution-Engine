527337ef898319709c72605fab3078f36429a51f
Caelan Hill <caelan.hill@gmail.com>
2024-08-07 17:55:11 +0200

    feat: added getIndivdual workflow

--- api/handlers/workflow.go
+++ api/handlers/workflow.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"api/models"
 	"api/utilities"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func SetupWorkflowRoutes(g *gin.RouterGroup, h Workflow) {
@@ -21,13 +23,31 @@ func (w Workflow) GetWorkflows(c *gin.Context) {
 	result := w.DB.Find(&workflows)
 	if result.Error != nil {
 		logger.Error(result.Error)
-		c.JSON(500, gin.H{"error": "Internal Server Error"})
+		c.JSON(http.StatusInternalServerError, models.Response{Error: "Internal Server Error"})
 		return
 	}
-	c.JSON(200, gin.H{"data": workflows})
+	c.JSON(http.StatusOK, models.Response{Data: workflows})
 }
 
+
 func (w Workflow) GetIndivualWorkflow(c *gin.Context) {
+	logger := utilities.NewLogger().LogWithCaller()
+	id := c.Param("id")
+
+	var workflow models.Workflow
+	result := w.DB.First(&workflow, id)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			logger.Warnf("Workflow with ID %s not found", id)
+			c.JSON(http.StatusOK, models.Response{Data: nil})
+		} else {
+			logger.Error(result.Error)
+			c.JSON(http.StatusInternalServerError, models.Response{Error: "Internal Server Error"})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{Data: workflow})
 }
 
 func (w Workflow) StoreWorkflow(c *gin.Context) {

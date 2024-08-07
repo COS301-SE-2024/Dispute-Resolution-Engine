4a1fdb858dcec912a6bb19fad266afa3c6352b22
Caelan Hill <caelan.hill@gmail.com>
2024-08-07 17:37:24 +0200

    feat: added routing setup in API

--- api/handlers/workflow.go
+++ api/handlers/workflow.go
@@ -1,2 +1,27 @@
 package handlers
 
+import "github.com/gin-gonic/gin"
+
+func SetupWorkflowRoutes(g *gin.RouterGroup, h Workflow) {
+	g.GET("", h.GetWorkflows)
+	g.GET("/:id", h.GetIndivualWorkflow)
+	g.POST("", h.StoreWorkflow)
+	g.PUT("/:id", h.UpdateWorkflow)
+	g.DELETE("/:id", h.DeleteWorkflow)
+}
+
+func (w Workflow) GetWorkflows(c *gin.Context) {
+}
+
+func (w Workflow) GetIndivualWorkflow(c *gin.Context) {
+}
+
+func (w Workflow) StoreWorkflow(c *gin.Context) {
+}
+
+func (w Workflow) UpdateWorkflow(c *gin.Context) {
+}
+
+func (w Workflow) DeleteWorkflow(c *gin.Context) {
+}
+

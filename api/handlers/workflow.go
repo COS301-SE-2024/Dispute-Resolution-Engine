4dafcb33668e04156654873ccb72772076e091ae
Caelan Hill <caelan.hill@gmail.com>
2024-08-07 17:49:21 +0200

    feat: implimented getAll workflows function

--- api/handlers/workflow.go
+++ api/handlers/workflow.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/models"
+	"api/utilities"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupWorkflowRoutes(g *gin.RouterGroup, h Workflow) {
 	g.GET("", h.GetWorkflows)
@@ -11,7 +16,15 @@ func SetupWorkflowRoutes(g *gin.RouterGroup, h Workflow) {
 }
 
 func (w Workflow) GetWorkflows(c *gin.Context) {
-	
+	logger := utilities.NewLogger().LogWithCaller()
+	var workflows []models.Workflow
+	result := w.DB.Find(&workflows)
+	if result.Error != nil {
+		logger.Error(result.Error)
+		c.JSON(500, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	c.JSON(200, gin.H{"data": workflows})
 }
 
 func (w Workflow) GetIndivualWorkflow(c *gin.Context) {

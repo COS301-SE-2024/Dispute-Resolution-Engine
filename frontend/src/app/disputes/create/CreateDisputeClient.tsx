1e79c4cb92942c55c34dd55f1583577fcb28b108
Vincent <vafdaf11@gmail.com>
2024-08-09 17:44:27 +0200

    feat: Add error messages to dispute create form

--- frontend/src/app/disputes/create/CreateDisputeClient.tsx
+++ frontend/src/app/disputes/create/CreateDisputeClient.tsx
@@ -1,23 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { DisputeCreateData, disputeCreateSchema } from "@/lib/schema/dispute";
 import { useId, useRef, useState } from "react";
 import { createDispute } from "@/lib/actions/dispute";
 import { Textarea } from "@/components/ui/textarea";
 import FileInput from "@/components/form/file-input";
+import { FormField, FormMessage } from "@/components/ui/form-client";
+
+const CreateMessage = FormMessage<DisputeCreateData>;
+const CreateField = FormField<DisputeCreateData>;
 
 export default function CreateDisputeClient() {
   const form = useForm<DisputeCreateData>({
@@ -31,6 +27,8 @@ export default function CreateDisputeClient() {
     resolver: zodResolver(disputeCreateSchema),
   });
 
+  const { register } = form;
+
   const formRef = useRef(null);
   const filesId = useId();
 
@@ -43,8 +41,16 @@ export default function CreateDisputeClient() {
     createDispute(null, formdata);
   };
 
+  const resNameId = useId();
+  const resEmailId = useId();
+  const resTelId = useId();
+
+  const titleId = useId();
+  const summaryId = useId();
+  const fileId = useId();
+
   return (
-    <Form {...form}>
+    <FormProvider {...form}>
       <form ref={formRef} onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 px-10 py-5">
         <Card>
           <CardHeader>
@@ -54,42 +60,23 @@ export default function CreateDisputeClient() {
             <CardDescription>Who are you filing a dispute against?</CardDescription>
           </CardHeader>
           <CardContent className="space-y-5">
-            <FormField
-              control={form.control}
-              name="respondentName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Respondent Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John Doe" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="respondentEmail"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>RespondentEmail</FormLabel>
-                  <FormControl>
-                    <Input placeholder="abc@example.com" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="respondentTelephone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Respondent Telephone</FormLabel>
-                  <FormControl>
-                    <Input placeholder="012 345 6789" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+            <CreateField id={resNameId} name="respondentName" label="Respondent Name">
+              <Input id={resNameId} placeholder="John Doe" {...register("respondentName")} />
+            </CreateField>
+            <CreateField id={resEmailId} name="respondentEmail" label="Respondent Email">
+              <Input
+                id={resEmailId}
+                placeholder="abc@example.com"
+                {...register("respondentEmail")}
+              />
+            </CreateField>
+            <CreateField id={resTelId} name="respondentTelephone" label="Respondent Telephone">
+              <Input
+                id={resTelId}
+                placeholder="012 345 6789"
+                {...register("respondentTelephone")}
+              />
+            </CreateField>
           </CardContent>
         </Card>
         <Card className="w-full">
@@ -100,49 +87,23 @@ export default function CreateDisputeClient() {
             <CardDescription>What is the dispute about?</CardDescription>
           </CardHeader>
           <CardContent className="space-y-5">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Dispute Title" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="summary"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Summary</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="The aforementioned party..." {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="file"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Evidence</FormLabel>
-                    <FormControl>
-                      <FileInput onValueChange={setFiles} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
+            <CreateField id={titleId} name="title" label="Dispute Title">
+              <Input id={titleId} placeholder="Dispute Title" {...register("title")} />
+            </CreateField>
+            <CreateField id={summaryId} name="summary" label="Dispute Summary">
+              <Textarea
+                id={summaryId}
+                placeholder="A short description of why you are filing a dispute (max. 500 words)"
+                {...register("summary")}
+              />
+            </CreateField>
+            <CreateField id={fileId} name="file" label="Evidence">
+              <FileInput id={fileId} onValueChange={setFiles} />
+            </CreateField>
             <Button type="submit">Create</Button>
           </CardContent>
         </Card>
       </form>
-    </Form>
+    </FormProvider>
   );
 }

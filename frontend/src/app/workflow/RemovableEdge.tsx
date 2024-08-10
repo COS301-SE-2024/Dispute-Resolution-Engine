118562dcfec96794ebde167beebfd17f5dae7a0d
Michael Horwitz <michael@horwitz.co.za>
2024-08-10 15:27:43 +0200

    feat: attempt at a custom edge

---
+++ frontend/src/app/workflow/RemovableEdge.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { BaseEdge, getStraightPath } from "@xyflow/react";
+
+
+export default function RemovableEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+}: {
+  id: string;
+  sourceX: number;
+  sourceY: number;
+  targetX: number;
+  targetY: number;
+}) {
+  const [edgePath] = getStraightPath({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+  });
+
+  return (
+    <>
+      <BaseEdge id={id} path={edgePath} />
+    </>
+  );
+}
--- frontend/src/app/workflow/page.tsx
+++ frontend/src/app/workflow/page.tsx
@@ -1,14 +1,26 @@
-import { ReactFlow } from "@xyflow/react";
+"use client"
+import { Background, BaseEdge, Controls, getStraightPath, ReactFlow } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import RemovableEdge from "./RemovableEdge";
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "Dispute Created" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "Response Due" } },
+  { id: "3", position: { x: 0, y: 200 }, data: { label: "Default Judgement" } },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e2-3", source: "2", target: "3", type: "removable-edge" },
+];
+const edgeTypes = {
+  "removable-edge": RemovableEdge,
+};
 
 export default function Workflow() {
   return (
-    <div className="w-screen h-screen">
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <div className="w-full h-full">
+      <ReactFlow colorMode="system" nodes={initialNodes} edges={initialEdges} edgeTypes={edgeTypes}>
+        <Controls />
+      </ReactFlow>
     </div>
   );
 }

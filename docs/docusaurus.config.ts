5543183a691565035ec2fd134bb20d657d5d1fbd
Michael Horwitz <michael@horwitz.co.za>
2024-08-10 23:11:43 +0200

    feat: messing with config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "cos301-se-2024", // Usually your GitHub org/user name.
   projectName: "Dispute-Resolution-Engine", // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: undefined,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",

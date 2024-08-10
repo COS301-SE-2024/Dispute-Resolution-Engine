c1b1c7e27121f29bc746435ba1fa144e7504c589
Michael Horwitz <michael@horwitz.co.za>
2024-08-10 23:08:06 +0200

    feat: changed config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,8 +15,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "cos301-se-2024", // Usually your GitHub org/user name.
+  projectName: "Dispute-Resolution-Engine", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",

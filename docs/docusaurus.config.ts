527851e20e1105aa99dc87ad1f3dd7d44cc393e5
Michael Horwitz <michael@horwitz.co.za>
2024-08-10 23:02:01 +0200

    feat: changed baseURL

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://capstone-dre.dns.net.za",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/Dispute-Resolution-Engine/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

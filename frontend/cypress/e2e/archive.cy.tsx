a849822260a454253dee113de668ffcb4d8c5eb0
Michael Horwitz <michael@horwitz.co.za>
2024-08-11 17:12:06 +0200

    feat: Fleshed out Archive test

--- frontend/cypress/e2e/archive.cy.tsx
+++ frontend/cypress/e2e/archive.cy.tsx
@@ -1,6 +1,9 @@
 describe('Archive', () => {
 	it('should be accessible', () => {
 		cy.visit('https://capstone-dre.dns.net.za/archive')
-        cy.get('h1').contains('Resolved Disputes')
+        cy.get('a').contains('Read More').click()
+		cy.contains('Date Filed')
+		cy.contains('Decision')
+		cy.get('dd').contains('2024')
 	})	
   })

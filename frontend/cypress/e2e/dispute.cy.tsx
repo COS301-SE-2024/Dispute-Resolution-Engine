14a1d14128c1488d7e0a176c1e979420fd234675
Michael Horwitz <michael@horwitz.co.za>
2024-08-11 13:21:49 +0200

    feat: updated to use test.txt

--- frontend/cypress/e2e/dispute.cy.tsx
+++ frontend/cypress/e2e/dispute.cy.tsx
@@ -1,50 +1,56 @@
-import 'cypress-file-upload'
-describe('Navigation', () => {
-	//TODO: only login if not logged in
-	let cookie : string | undefined
-	it('should login', () => {
-		cy.visit('https://capstone-dre.dns.net.za/login')
-		cy.contains('Email').type('sediv39443@alientex.com')
-		cy.contains('Password').type('Password1234#')
-		cy.get('button').contains('Login').click()
-		cy.visit('http://capstone-dre.dns.net.za/disputes')
-		cy.getCookie('jwt').then(tempCookie => {
-			cookie = tempCookie?.value
-		})
-	})
-	let time = Date.now()
-	let title = 'Cypress Test Title' + time
-	let description = 'I am creating a cypress test'
-	it('should be able to create a dispute', () => {
-		cy.setCookie('jwt', cookie ?? '')
-		cy.visit('http://capstone-dre.dns.net.za/disputes')
-		cy.get('a').contains('+ Create').click()
-		cy.get('input[name="respondentName"]').click().type('Bob Charlie')
-		cy.get('input[name="respondentEmail"]').click().type('sediv39443@alientex.com')
-		cy.get('input[name="respondentTelephone"]').click().type('0123456789')
-		cy.get('input[name="title"]').click().type(title)
-		cy.get('textarea[name="summary"').click().type(description)
-		cy.fixture('testpicture.png').then(fileContent => {
-			cy.get('input[type="file"]').attachFile({
-				fileContent: fileContent.toString(),
-				fileName: 'testpicture.png',
-				mimeType: 'image/png'
-			});
-		});
-		cy.get('button[type=submit]').click()
-		cy.get('span').contains(title)
-	})
-	it('should be able see the dispute', () => {
-		cy.setCookie('jwt', cookie ?? '')
-		cy.visit('http://capstone-dre.dns.net.za/disputes')
-		cy.get('span').contains(title).click()
-		cy.get('h1').contains(title)
-		cy.get('p').contains(description)
-	})
-	it('should be in the archive', () => {
-		cy.setCookie('jwt', cookie ?? '')
-		cy.visit('http://capstone-dre.dns.net.za/archive')
-		cy.get('input[name="q"]').click().type(title)
-		cy.get('button[type="submit"]').click()
-	})
-  })
+import "cypress-file-upload";
+describe("Navigation", () => {
+  let cookie: string | undefined;
+  // it('should login', () => {
+  // 	cy.visit('https://capstone-dre.dns.net.za/login')
+  // 	cy.contains('Email').type('sediv39443@alientex.com')
+  // 	cy.contains('Password').type('Password1234#')
+  // 	cy.get('button').contains('Login').click()
+  // 	cy.visit('http://capstone-dre.dns.net.za/disputes')
+  // 	cy.getCookie('jwt').then(tempCookie => {
+  // 		cookie = tempCookie?.value
+  // 	})
+  // })
+  let time = Date.now();
+  let title = "Cypress Test Title" + time;
+  let description = "I am creating a cypress test";
+  it("should be able to create a dispute", () => {
+    cy.visit("https://capstone-dre.dns.net.za/login");
+    cy.contains("Email").type("sediv39443@alientex.com");
+    cy.contains("Password").type("Password1234#");
+    cy.get("button").contains("Login").click();
+    cy.wait(2000);
+    cy.getCookie("jwt").then((tempCookie) => {
+      cookie = tempCookie?.value;
+    });
+    cy.visit("http://capstone-dre.dns.net.za/disputes");
+    cy.get("a").contains("+ Create").click();
+    cy.get('input[name="respondentName"]').click().type("Bob Charlie");
+    cy.get('input[name="respondentEmail"]').click().type("sediv39443@alientex.com");
+    cy.get('input[name="respondentTelephone"]').click().type("0123456789");
+    cy.get('input[name="title"]').click().type(title);
+    cy.get('textarea[name="summary"').click().type(description);
+    cy.fixture("test.txt").then((fileContent) => {
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "test.txt",
+        mimeType: "image/png",
+      });
+    });
+    cy.get(".pt-0 > .inline-flex").click();
+    cy.get("span").contains(title);
+  });
+  it("should be able see the dispute", () => {
+    cy.setCookie("jwt", cookie ?? "");
+    cy.visit("http://capstone-dre.dns.net.za/disputes");
+    cy.get("span").contains(title).click();
+    cy.get("h1").contains(title);
+    cy.get("p").contains(description);
+  });
+  it("should be in the archive", () => {
+    cy.setCookie("jwt", cookie ?? "");
+    cy.visit("http://capstone-dre.dns.net.za/archive");
+    cy.get('input[name="q"]').click().type(title);
+    cy.get('button[type="submit"]').click();
+  });
+});

3d07891f872bf2669703d807f33b53856f8ce85b
Vincent <vafdaf11@gmail.com>
2024-08-17 15:38:08 +0200

    feat: Implement basic layout page

--- frontend-svelte/src/routes/+layout.svelte
+++ frontend-svelte/src/routes/+layout.svelte
@@ -1,44 +1,45 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
 </script>
 
 <!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+<AppBar gap="gap-4">
+	<svelte:fragment slot="lead">
+		<a href="/" class="flex items-center gap-2">
+			<figure class="h-16">
+				<section class="img-bg" />
+				<img src="ralph.png" alt="DRE Mascot" class="aspect-square h-16" />
+			</figure>
+			<strong title="Dispute Resolution Engine" class="text-xl uppercase">DRE</strong>
+		</a>
 	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+	<nav>
+		<a class="btn variant-soft-surface" href="/archive"> Archive </a>
+		<a
+			class="btn variant-soft-surface"
+			href="https://cos301-se-2024.github.io/Dispute-Resolution-Engine/"
+		>
+			Help
+		</a>
+	</nav>
+	<svelte:fragment slot="trail">
+		<a class="btn btn-sm variant-ghost-surface" href="/profile" target="_blank" rel="noreferrer">
+			Profile
+		</a>
+	</svelte:fragment>
+</AppBar>
+<slot />
+
+<style lang="postcss">
+	figure {
+		@apply relative flex items-center justify-center;
+	}
+	figure img {
+		@apply z-10;
+	}
+	.img-bg {
+		@apply absolute bg-secondary-400 blur-[15px];
+		@apply w-8 h-8;
+	}
+</style>

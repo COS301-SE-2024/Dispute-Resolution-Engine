3ae39ddfda951dbf8e76973a57af20b8d974c593
Vincent <vafdaf11@gmail.com>
2024-08-17 15:02:45 +0200

    feat: Initialize SvelteKit project

---
+++ frontend-svelte/src/ralph.ts
@@ -0,0 +1,102 @@
+// You can also use the generator at https://skeleton.dev/docs/generator to create these values for you
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+export const ralph: CustomThemeConfig = {
+	name: 'ralph',
+	properties: {
+		// =~= Theme Properties =~=
+		'--theme-font-family-base': `system-ui`,
+		'--theme-font-family-heading': `system-ui`,
+		'--theme-font-color-base': '0 0 0',
+		'--theme-font-color-dark': 'var(--color-primary-50)',
+		'--theme-rounded-base': '4px',
+		'--theme-rounded-container': '8px',
+		'--theme-border-base': '0px',
+		// =~= Theme On-X Colors =~=
+		'--on-primary': '0 0 0',
+		'--on-secondary': '255 255 255',
+		'--on-tertiary': '0 0 0',
+		'--on-success': '0 0 0',
+		'--on-warning': '0 0 0',
+		'--on-error': '255 255 255',
+		'--on-surface': '255 255 255',
+		// =~= Theme Colors  =~=
+		// primary | #9beaf2
+		'--color-primary-50': '240 252 253', // #f0fcfd
+		'--color-primary-100': '235 251 252', // #ebfbfc
+		'--color-primary-200': '230 250 252', // #e6fafc
+		'--color-primary-300': '215 247 250', // #d7f7fa
+		'--color-primary-400': '185 240 246', // #b9f0f6
+		'--color-primary-500': '155 234 242', // #9beaf2
+		'--color-primary-600': '140 211 218', // #8cd3da
+		'--color-primary-700': '116 176 182', // #74b0b6
+		'--color-primary-800': '93 140 145', // #5d8c91
+		'--color-primary-900': '76 115 119', // #4c7377
+		// secondary | #336cd4
+		'--color-secondary-50': '224 233 249', // #e0e9f9
+		'--color-secondary-100': '214 226 246', // #d6e2f6
+		'--color-secondary-200': '204 218 244', // #ccdaf4
+		'--color-secondary-300': '173 196 238', // #adc4ee
+		'--color-secondary-400': '112 152 225', // #7098e1
+		'--color-secondary-500': '51 108 212', // #336cd4
+		'--color-secondary-600': '46 97 191', // #2e61bf
+		'--color-secondary-700': '38 81 159', // #26519f
+		'--color-secondary-800': '31 65 127', // #1f417f
+		'--color-secondary-900': '25 53 104', // #193568
+		// tertiary | #0EA5E9
+		'--color-tertiary-50': '219 242 252', // #dbf2fc
+		'--color-tertiary-100': '207 237 251', // #cfedfb
+		'--color-tertiary-200': '195 233 250', // #c3e9fa
+		'--color-tertiary-300': '159 219 246', // #9fdbf6
+		'--color-tertiary-400': '86 192 240', // #56c0f0
+		'--color-tertiary-500': '14 165 233', // #0EA5E9
+		'--color-tertiary-600': '13 149 210', // #0d95d2
+		'--color-tertiary-700': '11 124 175', // #0b7caf
+		'--color-tertiary-800': '8 99 140', // #08638c
+		'--color-tertiary-900': '7 81 114', // #075172
+		// success | #15cb5b
+		'--color-success-50': '220 247 230', // #dcf7e6
+		'--color-success-100': '208 245 222', // #d0f5de
+		'--color-success-200': '197 242 214', // #c5f2d6
+		'--color-success-300': '161 234 189', // #a1eabd
+		'--color-success-400': '91 219 140', // #5bdb8c
+		'--color-success-500': '21 203 91', // #15cb5b
+		'--color-success-600': '19 183 82', // #13b752
+		'--color-success-700': '16 152 68', // #109844
+		'--color-success-800': '13 122 55', // #0d7a37
+		'--color-success-900': '10 99 45', // #0a632d
+		// warning | #EAB308
+		'--color-warning-50': '252 244 218', // #fcf4da
+		'--color-warning-100': '251 240 206', // #fbf0ce
+		'--color-warning-200': '250 236 193', // #faecc1
+		'--color-warning-300': '247 225 156', // #f7e19c
+		'--color-warning-400': '240 202 82', // #f0ca52
+		'--color-warning-500': '234 179 8', // #EAB308
+		'--color-warning-600': '211 161 7', // #d3a107
+		'--color-warning-700': '176 134 6', // #b08606
+		'--color-warning-800': '140 107 5', // #8c6b05
+		'--color-warning-900': '115 88 4', // #735804
+		// error | #d2193e
+		'--color-error-50': '248 221 226', // #f8dde2
+		'--color-error-100': '246 209 216', // #f6d1d8
+		'--color-error-200': '244 198 207', // #f4c6cf
+		'--color-error-300': '237 163 178', // #eda3b2
+		'--color-error-400': '224 94 120', // #e05e78
+		'--color-error-500': '210 25 62', // #d2193e
+		'--color-error-600': '189 23 56', // #bd1738
+		'--color-error-700': '158 19 47', // #9e132f
+		'--color-error-800': '126 15 37', // #7e0f25
+		'--color-error-900': '103 12 30', // #670c1e
+		// surface | #2d3b86
+		'--color-surface-50': '224 226 237', // #e0e2ed
+		'--color-surface-100': '213 216 231', // #d5d8e7
+		'--color-surface-200': '203 206 225', // #cbcee1
+		'--color-surface-300': '171 177 207', // #abb1cf
+		'--color-surface-400': '108 118 170', // #6c76aa
+		'--color-surface-500': '45 59 134', // #2d3b86
+		'--color-surface-600': '41 53 121', // #293579
+		'--color-surface-700': '34 44 101', // #222c65
+		'--color-surface-800': '27 35 80', // #1b2350
+		'--color-surface-900': '22 29 66' // #161d42
+	}
+};
+
---
+++ frontend-svelte/tailwind.config.ts
@@ -0,0 +1,25 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { ralph } from './src/ralph'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				custom: [
+					ralph,
+				],
+			},
+		}),
+	],
+} satisfies Config;

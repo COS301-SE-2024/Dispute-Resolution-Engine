60b6195dc36185c6a8d7432d03a8e8ff4336b10c
Michael Horwitz <michael@horwitz.co.za>
2024-08-26 18:54:45 +0200

    feat: Changed to form and minor refactor

--- frontend/src/app/workflow/page.tsx
+++ frontend/src/app/workflow/page.tsx
@@ -1,63 +1,84 @@
-"use client"
-import { useCallback, useRef, useState } from 'react';
-import {
-  ReactFlow,
-  addEdge,
-  useNodesState,
-  useEdgesState,
-} from '@xyflow/react';
-import CustomEdge from './CustomEdge';
+"use client";
+import { useCallback, useRef, useState } from "react";
+import { ReactFlow, addEdge, useNodesState, useEdgesState } from "@xyflow/react";
+import CustomEdge from "./CustomEdge";
 
-import '@xyflow/react/dist/style.css';
-import { Button } from '@/components/ui/button';
+import "@xyflow/react/dist/style.css";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { register } from "module";
+import { Textarea } from "@/components/ui/textarea";
 
 const initialNodes = [
-  { id: 'a', position: { x: 0, y: 0 }, data: { label: 'Node A' } },
-  { id: 'b', position: { x: 0, y: 100 }, data: { label: 'Node B' } },
-  { id: 'c', position: { x: 0, y: 200 }, data: { label: 'Node C' } },
+  { id: "a", position: { x: 0, y: 0 }, data: { label: "Node A" } },
+  { id: "b", position: { x: 0, y: 100 }, data: { label: "Node B" } },
+  { id: "c", position: { x: 0, y: 200 }, data: { label: "Node C" } },
 ];
 
 const initialEdges = [
-  { id: 'a->b', type: 'custom-edge', source: 'a', target: 'b' },
-  { id: 'b->c', type: 'custom-edge', source: 'b', target: 'c' },
+  { id: "a->b", type: "custom-edge", source: "a", target: "b" },
+  { id: "b->c", type: "custom-edge", source: "b", target: "c" },
 ];
 
 const edgeTypes = {
-  'custom-edge': CustomEdge,
+  "custom-edge": CustomEdge,
 };
-// http://localhost:3000/workflow 
+// http://localhost:3000/workflow
 function Flow() {
-  let currId = useRef(1)
+  let currId = useRef(1);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect = useCallback(
-    (connection : any) => {
-      const edge = { ...connection, type: 'custom-edge' };
+    (connection: any) => {
+      const edge = { ...connection, type: "custom-edge" };
       setEdges((eds) => addEdge(edge, eds));
     },
-    [setEdges],
+    [setEdges]
   );
-  const addNode  = useCallback(() => {
-    const newNode  = { id: currId.current.toString(), position: { x: 0, y: 200 }, data: { label: 'NEW NODE YIPPEE ' + currId.current.toString() } }
-    currId.current = currId.current + 1
-    setNodes((nds) => nds.concat(newNode));  
-  
+  const addNode = useCallback((params : any) => {
+    const newNode = {
+      id: currId.current.toString(),
+      position: { x: 0, y: 200 },
+      data: { label: params.label},
+    };
+    currId.current = currId.current + 1;
+    setNodes((nds) => nds.concat(newNode));
   }, [setNodes]);
+  const newNodeSchema = z.object({
+    label: z.string().min(1).max(50),
+  });
+  type NewNodeData = z.infer<typeof newNodeSchema>;
+  const form = useForm<NewNodeData>({
+    defaultValues: {
+      label: "New Node",
+    },
+    resolver: zodResolver(newNodeSchema),
+  });
+  const onSubmit = () => {};
+  const formRef = useRef(null);
   return (
     <div className="h-96">
-    <ReactFlow className="h-24"
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      edgeTypes={edgeTypes}
-      colorMode='dark'
-      fitView
-    >
-
-    </ReactFlow>
-    <Button onClick={addNode}>ADD NODE</Button>
+      <ReactFlow
+        className="h-24"
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        edgeTypes={edgeTypes}
+        colorMode="dark"
+        fitView
+      ></ReactFlow>
+      <FormProvider {...form}>
+        <form ref={formRef} onSubmit={form.handleSubmit(addNode)}>
+          <Textarea id={"placeholderID"} {...form.register("label")}></Textarea>
+          <Button type="submit">ADD NODE</Button>
+        </form>
+      </FormProvider>
     </div>
   );
 }

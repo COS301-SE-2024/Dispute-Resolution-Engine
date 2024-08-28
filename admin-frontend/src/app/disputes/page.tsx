e6ad6bd5b48c14bfcacc627ec4e01d00615eb48c
Vincent <vafdaf11@gmail.com>
2024-08-28 15:03:35 +0200

    feat: Make search icon overlap with search bar

--- admin-frontend/src/app/disputes/page.tsx
+++ admin-frontend/src/app/disputes/page.tsx
@@ -8,71 +8,25 @@ export default function Disputes() {
         <h2 className="p-5 font-bold tracking-wide text-xl border-b dark:border-primary-500/30 border-primary-500/20">
           Disputes
         </h2>
-        <div class="flex items-center pl-5 pr-2 border-b dark:border-primary-500/30 border-primary-500/20">
-          <Search className="pointer-events-none" size={20} />
-          <input
-            type="search"
-            className="grow p-5 bg-transparent"
-            placeholder="Search disputes..."
-          />
-          <Button variant="ghost">
+        <div class="flex items-center px-5 gap-2 pr-2 border-b dark:border-primary-500/30 border-primary-500/20">
+          <div className="grid grid-cols-[auto_1fr] items-center grow">
+            <input
+              type="search"
+              className="col-span-2 p-5 bg-transparent  col-start-1 row-start-1 pl-12"
+              placeholder="Search disputes..."
+            />
+            <div className="p-5 row-start-1 col-start-1 pointer-events-none">
+              <Search size={20} />
+            </div>
+          </div>
+          <Button variant="ghost" className="gap-2">
             <Filter />
             <span>Filter by</span>
           </Button>
         </div>
       </header>
       <main className="overflow-auto p-5 grow">
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
+        <h1>Insert table here</h1>
       </main>
     </div>
   );

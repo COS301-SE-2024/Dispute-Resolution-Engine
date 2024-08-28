8e7c30c12cff4f4e82dbcf9777ad9168cef1e823
Vincent <vafdaf11@gmail.com>
2024-08-28 13:18:18 +0200

    feat: Tweak styling of buttons to be more theme-"compliant"

--- admin-frontend/src/components/admin/nav-link.tsx
+++ admin-frontend/src/components/admin/nav-link.tsx
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
 
 export default function NavLink({
   label,
@@ -17,7 +18,7 @@ export default function NavLink({
 }) {
   const pathname = usePathname();
   const className = cn(
-    "p-3 h-auto rounded-xl transition-all",
+    "p-3 rounded-xl transition-all",
     href == pathname &&
       "hover:shadow-lg hover:shadow-primary-600/35 text-primary-500 dark:text-secondary-500 border border-primary-500/35 bg-primary-500/20",
     expanded && "w-full",
--- admin-frontend/src/components/ui/button.tsx
+++ admin-frontend/src/components/ui/button.tsx
@@ -5,32 +5,24 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300",
+  "inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
   {
     variants: {
       variant: {
         default:
-          "bg-dre-200 text-zinc-50 hover:bg-dre-200/90 dark:bg-dre-200 dark:text-white dark:hover:bg-dre-200/90",
+          "bg-primary-500 text-slate-50 hover:bg-dre-200/90 dark:bg-dre-200 dark:text-white dark:hover:bg-dre-200/90",
         destructive:
-          "bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
+          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
-          "border border-dre-200/50 bg-white hover:bg-dre-100/10 hover:text-zinc-900 dark:border-dre-100/20 dark:hover:border-dre-100/40 dark:bg-transparent dark:hover:bg-dre-bg-light/10 dark:hover:text-white ",
+          "border border-dre-200/50 bg-white hover:bg-dre-100/10 hover:text-slate-900 dark:border-dre-100/20 dark:hover:border-dre-100/40 dark:bg-transparent dark:hover:bg-dre-bg-light/10 dark:hover:text-white ",
         secondary:
-          "bg-zinc-100 text-zinc-900 hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
-        ghost:
-          "hover:bg-dre-100/50 hover:text-zinc-900 dark:hover:bg-dre-200/50 dark:hover:text-zinc-50",
-        link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+        ghost: "hover:bg-primary-400/10 hover:text-primary-800 dark:hover:text-primary-50",
+        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
     },
   },
 );

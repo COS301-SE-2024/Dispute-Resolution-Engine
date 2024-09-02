7097f7ffd561227a4ec504eba08bda4888910ddd
Michael Horwitz <michael@horwitz.co.za>
2024-09-02 12:00:09 +0200

    feat: dynamic checkbox

--- frontend/src/app/workflow/TimerCheckbox.tsx
+++ frontend/src/app/workflow/TimerCheckbox.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
-import { disputeDuration } from "@/lib/types";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@radix-ui/react-accordion";
-import { useState } from "react";
+} from "@/components/ui/accordion";
+
+type disputeDuration = {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
 
 export default function TimerCheckbox(data: any) {
   const [duration, setDuration] = useState<disputeDuration>({
@@ -17,7 +23,8 @@ export default function TimerCheckbox(data: any) {
     minutes: 20,
     seconds: 30,
   });
-  // data.data.duration = duration;
+  const [isChecked, setIsChecked] = useState(false);
+
   const handleInputChange = (
     evt: React.ChangeEvent<HTMLInputElement>,
     unit: keyof disputeDuration
@@ -28,40 +35,54 @@ export default function TimerCheckbox(data: any) {
       [unit]: value,
     }));
   };
+
+  const handleCheckboxChange: React.FormEventHandler<HTMLButtonElement> = (evt) => {
+    const target = evt.currentTarget as HTMLInputElement;
+    setIsChecked(target.checked);
+  };
+
   const arr = ["days", "hours", "minutes", "seconds"];
-  const inputs = arr.map((currInput) => {
+  const inputs = arr.map((currInput) => (
+    <div key={currInput}>
+      <Label htmlFor={currInput + "Inp"}>
+        {currInput.charAt(0).toUpperCase() + currInput.slice(1)}
+      </Label>
+      <Input
+        id={currInput + "Inp"}
+        type="number"
+        className="h-2 w-16 p-3"
+        value={duration[currInput as keyof disputeDuration]}
+        onChange={(evt) => handleInputChange(evt, currInput as keyof disputeDuration)}
+      />
+    </div>
+  ));
+  const timerText = () => {
     return (
-      <div key={currInput}>
-        <Label htmlFor={currInput + "Inp"}>
-          {currInput.charAt(0).toUpperCase() + currInput.slice(1)}
-        </Label>
-        <Input
-          id={currInput + "Inp"}
-          type="number"
-          className="h-2 w-16 p-3"
-          value={duration[currInput as keyof disputeDuration]}
-          onChange={(evt) => handleInputChange(evt, currInput as keyof disputeDuration)}
-        />
+      <div>
+        Timer: {duration.days == 0 ? "" : duration.days.toString() + "d"}{" "}
+        {duration.hours == 0 ? "" : duration.hours.toString() + "h"}{" "}
+        {duration.minutes == 0 ? "" : duration.minutes.toString() + "m"}{" "}
+        {duration.seconds == 0 ? "" : duration.seconds.toString() + "s"}
       </div>
     );
-  });
+  };
+
   return (
     <div className="grid grid-cols-[auto_1fr] gap-3">
-      <Checkbox id="timerCheckbox" />
+      <Checkbox id="timerCheckbox" checked={isChecked} onChange={handleCheckboxChange} />
       <Label htmlFor="timerCheckbox">
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="pt-0">
-              Timer: {duration.days == 0 ? "" : duration.days.toString() + "d"}{" "}
-              {duration.hours == 0 ? "" : duration.hours.toString() + "h"}{" "}
-              {duration.minutes == 0 ? "" : duration.minutes.toString() + "m"}{" "}
-              {duration.seconds == 0 ? "" : duration.seconds.toString() + "s"}
-            </AccordionTrigger>
-            <AccordionContent className="grid grid-cols-2 items-center gap-3 pt-3">
-              {inputs}
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+        {isChecked ? (
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="pt-0">{timerText()}</AccordionTrigger>
+              <AccordionContent className="grid grid-cols-2 items-center gap-3 pt-3">
+                {inputs}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ) : (
+          timerText()
+        )}
       </Label>
     </div>
   );

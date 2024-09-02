47e9632fedf254a9540d6794808d07fb786a9196
Vincent <vafdaf11@gmail.com>
2024-09-02 11:17:31 +0200

    feat: Tweak styling of table

--- admin-frontend/src/app/disputes/row.tsx
+++ admin-frontend/src/app/disputes/row.tsx
@@ -3,6 +3,7 @@
 import { StatusBadge, StatusDropdown } from "@/components/admin/status-dropdown";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { AdminDispute } from "@/lib/types/dispute";
+import { CornerDownRight, LinkIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function DisputeRow(props: AdminDispute) {
@@ -16,7 +17,7 @@ export default function DisputeRow(props: AdminDispute) {
   }
 
   return (
-    <TableRow>
+    <TableRow className="text-nowrap truncate">
       <TableCell className="font-medium">
         <Link href={{ pathname: "/disputes", query: { id: props.id } }}>{props.title}</Link>
       </TableCell>
@@ -28,7 +29,13 @@ export default function DisputeRow(props: AdminDispute) {
         </StatusDropdown>
       </TableCell>
       <TableCell>
-        <Link href={`/workflows/${props.workflow.id}`}>{props.workflow.title}</Link>
+        <Link
+          className="flex gap-1 items-center hover:underline text-nowrap"
+          href={`/workflows/${props.workflow.id}`}
+        >
+          <span>{props.workflow.title}</span>
+          <LinkIcon size="0.8rem" />
+        </Link>
       </TableCell>
       <TableCell className="text-center">{props.date_filed}</TableCell>
       <TableCell className="text-center">{props.date_resolved ?? "-"}</TableCell>

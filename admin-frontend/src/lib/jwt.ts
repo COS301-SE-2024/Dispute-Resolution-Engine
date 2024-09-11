ff5eb01e4ba56067857f274e2f37a3738d0add0b
Vincent <vafdaf11@gmail.com>
2024-09-11 19:15:39 +0200

    feat: Add middleware to redirect to login page

---
+++ admin-frontend/src/lib/jwt.ts
@@ -0,0 +1,21 @@
+export const JWT_VERIFY_TIMEOUT = 120;
+export const JWT_TIMEOUT = 60 * 20;
+export const JWT_KEY = "jwt";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export function getAuthToken(): string {
+  const jwt = cookies().get(JWT_KEY)?.value;
+  if (!jwt) {
+    redirect("/login");
+  }
+  return jwt;
+}
+
+export function setAuthToken(jwt: string, timeout: number = JWT_TIMEOUT) {
+  cookies().set(JWT_KEY, jwt, {
+    maxAge: timeout,
+    httpOnly: true,
+  });
+}

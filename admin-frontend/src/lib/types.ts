569b72267259e71961273ab7f0d1f9e31c3748c4
MichaelHorwitz <michael@horwitz.co.za>
2024-09-12 20:43:14 +0200

    feat: added types

---
+++ admin-frontend/src/lib/types.ts
@@ -0,0 +1,21 @@
+/**
+ * Generic Response from the API
+ */
+export type Result<T, E = string> =
+  | {
+      data: T;
+      error?: never;
+    }
+  | {
+      data?: never;
+      error: E;
+    };
+export type disputeDuration = {
+      days: number,
+      hours: number,
+      minutes: number,
+      seconds: number,
+    }
+export type eventType = {
+  id : string
+}

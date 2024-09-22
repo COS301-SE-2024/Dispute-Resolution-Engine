f713866ad196937455ed14339f5b1bef698b69bb
Caelan Hill <caelan.hill@gmail.com>
2024-09-22 22:33:47 +0200

    feat: added tests for timers

--- orchestrator/workflow/workflow_test.go
+++ orchestrator/workflow/workflow_test.go
@@ -18,3 +18,22 @@ func TestCreateTimer(t *testing.T) {
 	assert.Equal(t, onExpire, tmr.OnExpire, "The OnExpire trigger should match the provided value")
 }
 
+func TestSetDuration(t *testing.T) {
+	tmr := workflow.CreateTimer(5*time.Second, "trigger_event")
+	
+	newDuration := 20 * time.Second
+	tmr.SetDuration(newDuration)
+
+	assert.Equal(t, newDuration, tmr.GetDuration(), "The duration should be updated to 20s")
+}
+
+func TestGetDeadline(t *testing.T) {
+	duration := 5 * time.Second
+	tmr := workflow.CreateTimer(duration, "trigger_event")
+	
+	deadline := tmr.GetDeadline()
+
+	// The deadline should be roughly now + 5 seconds, we add some tolerance
+	assert.WithinDuration(t, time.Now().Add(duration), deadline, time.Millisecond*100, "The deadline should be current time + 5s")
+}
+

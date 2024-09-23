a50bec54e25690ce4a033ffb1d3f8db981b6cc4e
“S3BzA” <sebguim2002@gmail.com>
2024-09-23 15:34:02 +0200

    feat: update tickets table

--- initdb/init.sql
+++ initdb/init.sql
@@ -230,18 +230,22 @@ CREATE TABLE workflow_tags (
 
 ------------------------------------------------------------- TICKETING SYSTEM
 CREATE TYPE ticket_status_enum AS ENUM (
-	'Open',
-	'Closed',
-	'Solved',
-	'On Hold'
+    'Open',
+    'Closed',
+    'Solved',
+    'On Hold'
 );
 
 CREATE TABLE tickets (
-	id SERIAL PRIMARY KEY,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    subject VARCHAR(255) NOT NULL,
-    status ticket_status_enum NOT NULL
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Date the ticket was created
+    created_by BIGINT REFERENCES users(id) NOT NULL, -- User that created the ticket
+    dispute_id BIGINT REFERENCES disputes(id),       -- Dispute the ticket is related to
+    subject VARCHAR(255) NOT NULL,                   -- Subject to describe the ticket
+    status ticket_status_enum NOT NULL,              -- Status of the ticket
+    initial_message TEXT                             -- Body of the initial message of the ticket
 );
+
 
 CREATE TABLE ticket_messages (
 	id SERIAL PRIMARY KEY,

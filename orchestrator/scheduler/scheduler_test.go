eee98db1b741ea29289b5bb9bb648cd8931583ff
Caelan Hill <caelan.hill@gmail.com>
2024-09-23 16:51:04 +0200

    feat: add test for multiple starts

--- orchestrator/scheduler/scheduler_test.go
+++ orchestrator/scheduler/scheduler_test.go
@@ -134,3 +134,53 @@ func TestStart(t *testing.T) {
 
 	stop <- struct{}{}
 }
+func TestStartWithMultipleTimers(t *testing.T) {
+	logger := utilities.NewLogger()
+	s := scheduler.NewWithLogger(time.Second, logger)
+
+	stop := make(chan struct{})
+	result1 := make(chan bool)
+	result2 := make(chan bool)
+
+	timerName1 := "timer1"
+	deadline1 := time.Now().Add(500 * time.Millisecond)
+	event1 := func() {
+		result1 <- true
+	}
+
+	timerName2 := "timer2"
+	deadline2 := time.Now().Add(1 * time.Second)
+	event2 := func() {
+		result2 <- true
+	}
+
+	s.AddTimer(timerName1, deadline1, event1)
+	s.AddTimer(timerName2, deadline2, event2)
+	go s.Start(stop)
+
+	select {
+	case <-result1:
+		// Timer1 event was triggered
+		s.Lock.RLock()
+		_, exists := s.Timers[timerName1]
+		s.Lock.RUnlock()
+		assert.False(t, exists)
+	case <-time.After(2 * time.Second):
+		// Timer1 event was not triggered within the expected time
+		t.Fatal("Timer1 event was not triggered")
+	}
+
+	select {
+	case <-result2:
+		// Timer2 event was triggered
+		s.Lock.RLock()
+		_, exists := s.Timers[timerName2]
+		s.Lock.RUnlock()
+		assert.False(t, exists)
+	case <-time.After(2 * time.Second):
+		// Timer2 event was not triggered within the expected time
+		t.Fatal("Timer2 event was not triggered")
+	}
+
+	stop <- struct{}{}
+}

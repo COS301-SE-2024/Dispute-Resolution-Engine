78d35f3113c434150ed5a7893661d6e171da21d2
“S3BzA” <sebguim2002@gmail.com>
2024-09-24 17:36:52 +0200

    fix: take into account timezones in actove_workflows

--- initdb/init.sql
+++ initdb/init.sql
@@ -67,10 +67,10 @@ CREATE TABLE workflows (
 
 CREATE TABLE active_workflows (
 	id SERIAL PRIMARY KEY NOT NULL,
-    workflow BIGINT REFERENCES workflows(id) NOT NULL,
-    current_state VARCHAR(255),
-    date_submitted TIMESTAMP,
-    state_deadline TIMESTAMP,
+	workflow BIGINT REFERENCES workflows(id) NOT NULL,
+	current_state VARCHAR(255),
+	date_submitted TIMESTAMPTZ,  -- Includes timezone information
+	state_deadline TIMESTAMPTZ,  -- Includes timezone information
 	workflow_instance JSONB NOT NULL
 );
 

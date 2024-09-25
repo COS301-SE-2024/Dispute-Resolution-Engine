0cc40c27a40e9e74ffd08a1a46267ce1cbf05613
Caelan Hill <caelan.hill@gmail.com>
2024-09-25 20:36:21 +0200

    feat: added mocks for all other components

--- api/handlers/workflow/workflow_test.go
+++ api/handlers/workflow/workflow_test.go
@@ -3,6 +3,9 @@ package workflow_test
 import (
 	"api/models"
 	"errors"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 //------------------------------------------------------------------------- Mocks
@@ -102,6 +105,75 @@ func (m *mockDB) DeleteActiveWorkflow(wf *models.Workflow) error {
 	return nil
 }
 
+type mockJwtModel struct {
+	throwErrors bool
+}
+type mockEmailModel struct {
+	throwErrors bool
+}
+
+type mockAuditLogger struct {
+}
+
+
+// mock model auditlogger
+func (m *mockAuditLogger) LogDisputeProceedings(proceedingType models.EventTypes, eventData map[string]interface{}) error {
+	return nil
+}
+
+// mock model dispute
+
+func (m *mockJwtModel) GenerateJWT(user models.User) (string, error) {
+	if m.throwErrors {
+		return "", errors.ErrUnsupported
+	}
+	return "mock", nil
+}
+func (m *mockJwtModel) StoreJWT(email string, jwt string) error {
+	if m.throwErrors {
+		return errors.ErrUnsupported
+	}
+	return nil
+}
+func (m *mockJwtModel) GetJWT(email string) (string, error) {
+	if m.throwErrors {
+		return "", errors.ErrUnsupported
+	}
+	return "", nil
+}
+func (m *mockJwtModel) JWTMiddleware(c *gin.Context) {}
+
+func (m *mockJwtModel) GetClaims(c *gin.Context) (models.UserInfoJWT, error) {
+	if m.throwErrors {
+		return models.UserInfoJWT{}, errors.ErrUnsupported
+	}
+	return models.UserInfoJWT{
+		ID:                0,
+		FirstName:         "",
+		Surname:           "",
+		Birthdate:         time.Now(),
+		Nationality:       "",
+		Role:              "",
+		Email:             "",
+		PhoneNumber:       new(string),
+		AddressID:         new(int64),
+		Status:            "",
+		Gender:            "",
+		PreferredLanguage: new(string),
+		Timezone:          new(string),
+	}, nil
+
+}
+
+func (m *mockEmailModel) SendAdminEmail(c *gin.Context, disputeID int64, resEmail string, title string, summary string) {
+}
+
+func (m *mockEmailModel) SendDefaultUserEmail(c *gin.Context, email string, pass string, title string, summary string) {
+
+}
+
+func (m *mockEmailModel) NotifyDisputeStateChanged(c *gin.Context, disputeID int64, disputeStatus string) {
+}
 
 
 

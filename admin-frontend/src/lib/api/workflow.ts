1aabb6f21caffabe1e2299efe70dfddbbadf7186
Vincent <vafdaf11@gmail.com>
2024-09-26 11:10:35 +0200

    feat: Integrate workflow list things

--- admin-frontend/src/lib/api/workflow.ts
+++ admin-frontend/src/lib/api/workflow.ts
@@ -9,22 +9,58 @@ import {
   type WorkflowListResponse,
 } from "../types/workflow";
 
+// import { getAuthToken } from "../jwt";
+// import { API_URL, sf, validateResult } from "../utils";
+
 export async function createWorkflow(req: WorkflowCreateRequest): Promise<WorkflowCreateResponse> {
+  // return sf(`${API_URL}/workflows`, {
+  //   method: "POST",
+  //   body: JSON.stringify(req),
+  //   headers: {
+  //     Authorization: `Bearer ${getAuthToken()}`,
+  //   },
+  // }).then(validateResult<WorkflowCreateResponse>);
   throw new Error("not implemented");
 }
 
 export async function getWorkflowList(req: WorkflowListRequest): Promise<WorkflowListResponse> {
+  // return sf(`${API_URL}/workflows`, {
+  //   method: "POST",
+  //   body: JSON.stringify(req),
+  //   headers: {
+  //     Authorization: `Bearer ${getAuthToken()}`,
+  //   },
+  // }).then(validateResult<WorkflowListResponse>);
   throw new Error("not implemented");
 }
 
 export async function getWorkflowDetails(id: string): Promise<WorkflowDetailsResponse> {
+  // return sf(`${API_URL}/workflows/${id}`, {
+  //   method: "GET",
+  //   headers: {
+  //     Authorization: `Bearer ${getAuthToken()}`,
+  //   },
+  // }).then(validateResult<WorkflowDetailsResponse>);
   throw new Error("not implemented");
 }
 
-export async function updateWorkflow(req: WorkflowUpdateRequest): Promise<void> {
+export async function updateWorkflow(id: string, req: WorkflowUpdateRequest): Promise<void> {
+  // await sf(`${API_URL}/workflows/${id}`, {
+  //   method: "PATCH",
+  //   body: JSON.stringify(req),
+  //   headers: {
+  //     Authorization: `Bearer ${getAuthToken()}`,
+  //   },
+  // });
   throw new Error("not implemented");
 }
 
 export async function deleteWorkflow(id: string): Promise<void> {
+  // await sf(`${API_URL}/workflows/${id}`, {
+  //   method: "DELETE",
+  //   headers: {
+  //     Authorization: `Bearer ${getAuthToken()}`,
+  //   },
+  // });
   throw new Error("not implemented");
 }
--- admin-frontend/src/lib/utils.ts
+++ admin-frontend/src/lib/utils.ts
@@ -1,8 +1,45 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const API_URL = process.env.API_URL;
+
+export const resultSchema = z.object({
+  data: z.any().optional(),
+  error: z.string().optional(),
+});
+
+/**
+ * Parses the response body into JSON and checks that it conforms to the Result type
+ */
+export async function validateResult<T>(res: Response): Promise<T> {
+  const json = await res.json();
+  const { data, error } = resultSchema.safeParse(json);
+  if (error) {
+    console.log(error);
+    throw new Error(error.issues[0].message);
+  }
+  if (data.error) {
+    throw new Error(data.error);
+  }
+  return data.data! as T;
+}
+
+/**
+ * Wrapper around fetch that checks if the request was fine
+ */
+export function sf(
+  input: string | URL | globalThis.Request,
+  init?: RequestInit
+): Promise<Response> {
+  return fetch(input, init).then((res) => {
+    if (!res.ok) {
+      throw new Error(`Request failed with code ${res.status}`);
+    }
+    return res;
+  });
+}

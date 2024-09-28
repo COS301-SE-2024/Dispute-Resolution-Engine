2aea1c74a5d02a4cc63a23bd64b931daf5845cff
ZaguePrime <kivashin47@gmail.com>
2024-09-28 18:38:36 +0200

    feat: more signature changes and model mocks

--- api/handlers/ticket/ticket_test.go
+++ api/handlers/ticket/ticket_test.go
@@ -4,6 +4,8 @@ import (
 	"api/handlers/ticket"
 	"api/models"
 	"errors"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/suite"
@@ -34,66 +36,141 @@ func (suite *TicketErrorTestSuite) SetupTest() {
 	suite.ticketMock = &mockTicketModel{}
 	suite.jwtMock = &mockJwtModel{}
 	suite.envMock = &mockEnv{}
-	suite.router = gin.Default()
+	router := gin.Default()
 
 	handler := ticket.Ticket{Model: suite.ticketMock, JWT: suite.jwtMock, Env: suite.envMock}
+	router.Use(suite.jwtMock.JWTMiddleware)
+	router.POST("", handler.GetTicketList)
+	router.GET("/:id", handler.GetUserTicketDetails)
+	router.PATCH("/:id", handler.PatchTicketStatus)
+	router.POST("/:id/messages", handler.CreateTicketMessage)
+	router.POST("/create", handler.CreateTicket)
+
 }
 
 /*-------------------------------MOCK MODELS-----------------------------------------*/
-/*------------------------------MOCK TICKET MODEL-----------------------------------------*/
+//Ticket Mocks
 
-func (t *mockTicketModel) getAdminTicketList(searchTerm *string, limit *int, offset *int, sortAttr *models.Sort, filters *[]models.Filter) ([]models.TicketSummaryResponse, int64, error) {
+func (t *mockTicketModel) GetAdminTicketList(searchTerm *string, limit *int, offset *int, sortAttr *models.Sort, filters *[]models.Filter) ([]models.TicketSummaryResponse, int64, error) {
 	if t.throwErrors {
 		return nil, 0, errors.New("error")
 	}
 	return []models.TicketSummaryResponse{}, 0, nil
 }
 
-func (t *mockTicketModel) getTicketsByUserID(uid int64, searchTerm *string, limit *int, offset *int, sortAttr *models.Sort, filters *[]models.Filter) ([]models.TicketSummaryResponse, int64, error) {
+func (t *mockTicketModel) GetTicketsByUserID(uid int64, searchTerm *string, limit *int, offset *int, sortAttr *models.Sort, filters *[]models.Filter) ([]models.TicketSummaryResponse, int64, error) {
 	if t.throwErrors {
 		return nil, 0, errors.New("error")
 	}
 	return []models.TicketSummaryResponse{}, 0, nil
 }
 
-func (t *mockTicketModel) getTicketDetails(ticketID int64, userID int64) (models.TicketsByUser, error) {
+func (t *mockTicketModel) GetTicketDetails(ticketID int64, userID int64) (models.TicketsByUser, error) {
 	if t.throwErrors {
 		return models.TicketsByUser{}, errors.New("error")
 	}
 	return models.TicketsByUser{}, nil
 }
 
-func (t *mockTicketModel) getAdminTicketDetails(ticketID int64) (models.TicketsByUser, error) {
+func (t *mockTicketModel) GetAdminTicketDetails(ticketID int64) (models.TicketsByUser, error) {
 	if t.throwErrors {
 		return models.TicketsByUser{}, errors.New("error")
 	}
 	return models.TicketsByUser{}, nil
 }
 
-func (t *mockTicketModel) patchTicketStatus(status string, ticketID int64) error {
+func (t *mockTicketModel) PatchTicketStatus(status string, ticketID int64) error {
 	if t.throwErrors {
 		return errors.New("error")
 	}
 	return nil
 }
 
-func (t *mockTicketModel) addUserTicketMessage(ticketID int64, userID int64, message string) (models.TicketMessage, error) {
+func (t *mockTicketModel) AddUserTicketMessage(ticketID int64, userID int64, message string) (models.TicketMessage, error) {
 	if t.throwErrors {
 		return models.TicketMessage{}, errors.New("error")
 	}
 	return models.TicketMessage{}, nil
 }
 
-func (t *mockTicketModel) addAdminTicketMessage(ticketID int64, userID int64, message string) (models.TicketMessage, error) {
+func (t *mockTicketModel) AddAdminTicketMessage(ticketID int64, userID int64, message string) (models.TicketMessage, error) {
 	if t.throwErrors {
 		return models.TicketMessage{}, errors.New("error")
 	}
 	return models.TicketMessage{}, nil
 }
 
-func (t *mockTicketModel) createTicket(userID int64, dispute int64, subject string, message string) (models.Ticket, error) {
+func (t *mockTicketModel) CreateTicket(userID int64, dispute int64, subject string, message string) (models.Ticket, error) {
 	if t.throwErrors {
 		return models.Ticket{}, errors.New("error")
 	}
 	return models.Ticket{}, nil
+}
+
+// JWT Mocks
+func (m *mockJwtModel) GenerateJWT(user models.User) (string, error) {
+	if m.throwErrors {
+		return "", errors.ErrUnsupported
+	}
+	return "mock", nil
+}
+func (m *mockJwtModel) StoreJWT(email string, jwt string) error {
+	if m.throwErrors {
+		return errors.ErrUnsupported
+	}
+	return nil
+}
+func (m *mockJwtModel) GetJWT(email string) (string, error) {
+	if m.throwErrors {
+		return "", errors.ErrUnsupported
+	}
+	return "", nil
+}
+func (m *mockJwtModel) JWTMiddleware(c *gin.Context) {
+	if m.throwErrors {
+		c.JSON(http.StatusUnauthorized, models.Response{Error: "Unauthorized"})
+		c.Abort()
+		return
+	}
+	c.Next()
+}
+
+func (m *mockJwtModel) GetClaims(c *gin.Context) (models.UserInfoJWT, error) {
+	if m.throwErrors {
+		return models.UserInfoJWT{}, errors.ErrUnsupported
+	}
+	return models.UserInfoJWT{
+		ID:                0,
+		FirstName:         "",
+		Surname:           "",
+		Birthdate:         time.Now(),
+		Nationality:       "",
+		Role:              "",
+		Email:             "",
+		PhoneNumber:       new(string),
+		AddressID:         new(int64),
+		Status:            "",
+		Gender:            "",
+		PreferredLanguage: new(string),
+		Timezone:          new(string),
+	}, nil
+
+}
+
+//mock env
+
+func (m *mockEnv) LoadFromFile(files ...string) {
+}
+
+func (m *mockEnv) Register(key string) {
+}
+
+func (m *mockEnv) RegisterDefault(key, fallback string) {
+}
+
+func (m *mockEnv) Get(key string) (string, error) {
+	if m.throwErrors {
+		return "", m.Error
+	}
+	return "", nil
 }

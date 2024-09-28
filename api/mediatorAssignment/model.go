658baeb409e85314f07dd74da8bde2736722c667
Caelan Hill <caelan.hill@gmail.com>
2024-09-28 13:17:14 +0200

    feat: added db functions and table model

--- api/mediatorAssignment/model.go
+++ api/mediatorAssignment/model.go
@@ -1,10 +1,50 @@
 package mediatorassignment
 
-import "gorm.io/gorm"
+import (
+	"api/models"
+
+	"gorm.io/gorm"
+)
+
+const (
+	ExpertIDColumn            = "expert_id"
+	ExpertNameColumn          = "expert_name"
+	RejectionPercentageColumn = "rejection_percentage"
+	LastAssignedDateColumn    = "last_assigned_date"
+	AssignedDisputeCountColumn = "assigned_dispute_count"
+)
 
 type DBModel interface {
+	GetExpertSummaryViews() ([]models.ExpertSummaryView, error)
+	GetExpertSummaryViewByExpertID(expertID int) (models.ExpertSummaryView, error)
+	GetExpertSummaryViewByColumn(columnName string) (models.ExpertSummaryView, error)
+	GetExpertSummaryViewByColumnValue(columnName string, columnValue string) (models.ExpertSummaryView, error)
 }
 
 type DBModelReal struct {
 	DB *gorm.DB
+}
+
+func (d *DBModelReal) GetExpertSummaryViews() ([]models.ExpertSummaryView, error) {
+	var expertSummaryViews []models.ExpertSummaryView
+	d.DB.Find(&expertSummaryViews)
+	return expertSummaryViews, nil
+}
+
+func (d *DBModelReal) GetExpertSummaryViewByExpertID(expertID int) (models.ExpertSummaryView, error) {
+	var expertSummaryView models.ExpertSummaryView
+	d.DB.Where(ExpertIDColumn, expertID).First(&expertSummaryView)
+	return expertSummaryView, nil
+}
+
+func (d *DBModelReal) GetExpertSummaryViewByColumn(columnName string) (models.ExpertSummaryView, error) {
+	var expertSummaryView models.ExpertSummaryView
+	d.DB.Where(columnName).First(&expertSummaryView)
+	return expertSummaryView, nil
+}
+
+func (d *DBModelReal) GetExpertSummaryViewByColumnValue(columnName string, columnValue string) (models.ExpertSummaryView, error) {
+	var expertSummaryView models.ExpertSummaryView
+	d.DB.Where(columnName, columnValue).First(&expertSummaryView)
+	return expertSummaryView, nil
 }

efbf3cd081571f5e95715618ed5cd683d22e743a
Vincent <vafdaf11@gmail.com>
2024-09-28 16:04:07 +0200

    feat: Add better error messages

--- admin-frontend/src/app/(pages)/disputes/page.tsx
+++ admin-frontend/src/app/(pages)/disputes/page.tsx
@@ -16,13 +16,17 @@ import { useState } from "react";
 import DisputeFilters from "./dispute-filter";
 
 const searchSchema = z.object({
-  id: z.string().optional(),
+  id: z.coerce
+    .number({
+      message: "Invalid dispute ID",
+    })
+    .optional(),
 });
 
 export default function Disputes({ searchParams }: { searchParams: unknown }) {
   const { data: params, error: searchError } = searchSchema.safeParse(searchParams);
   if (!params) {
-    throw new Error(JSON.stringify(searchError));
+    throw new Error(searchError.issues[0].message);
   }
 
   const [client] = useState(new QueryClient());

f630f258bc4ec0136b9e3159d2772fb88e34b1df
ZaguePrime <kivashin47@gmail.com>
2024-09-28 20:05:55 +0200

    feat: another passing test

--- api/handlers/ticket/ticket_test.go
+++ api/handlers/ticket/ticket_test.go
@@ -468,3 +468,17 @@ func (suite *TicketErrorTestSuite) TestGetTicketListSuccess() {
 	suite.NoError(json.Unmarshal(w.Body.Bytes(), &result))
 	suite.NotEmpty(result.Data)
 }
+
+func (suite *TicketErrorTestSuite) TestGetTicketListSuccess2() {
+	body := `{"limit": 10, "offset": 0}`
+	req, _ := http.NewRequest("POST", "/tickets", bytes.NewBuffer([]byte(body)))
+	w := httptest.NewRecorder()
+	suite.router.ServeHTTP(w, req)
+
+	var result struct {
+		Data map[string]interface{} `json:"data"`
+	}
+	suite.Equal(http.StatusOK, w.Code)
+	suite.NoError(json.Unmarshal(w.Body.Bytes(), &result))
+	suite.NotEmpty(result.Data)
+}

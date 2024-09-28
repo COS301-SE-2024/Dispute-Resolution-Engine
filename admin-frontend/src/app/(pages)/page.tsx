99f6815d42b0165e3d80b436a3cb83f86c4a4cb8
Vincent <vafdaf11@gmail.com>
2024-09-28 10:56:53 +0200

    feat: Add status pie chart

--- admin-frontend/src/app/(pages)/page.tsx
+++ admin-frontend/src/app/(pages)/page.tsx
@@ -1,9 +1,37 @@
 import PageHeader from "@/components/admin/page-header";
+import StatusPieChart from "@/components/analytics/status-pie";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { DisputeStatus } from "@/lib/types";
+
+const data: Record<DisputeStatus, number> = {
+  "Awaiting Respondant": 1,
+  Active: 2,
+  Review: 3,
+  Settled: 4,
+  Refused: 5,
+  Withdrawn: 6,
+  Transfer: 7,
+  Appeal: 8,
+  Other: 9,
+};
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <PageHeader label="Dashboard" />
+      <div className="grow md:p-10 md:gap-10 overflow-y-auto flex flex-wrap  items-start justify-start">
+        <StatusPieChart data={data} />
+        <StatusPieChart data={data} />
+        <StatusPieChart data={data} />
+        <StatusPieChart data={data} />
+      </div>
     </div>
   );
 }
---
+++ admin-frontend/src/components/analytics/status-pie.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import * as React from "react";
+import { TrendingUp } from "lucide-react";
+import { Label, Legend, Pie, PieChart } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { DISPUTE_STATUS, DisputeStatus } from "@/lib/types";
+
+export const description = "A donut chart with text";
+
+const ACTIVE_STATUS: DisputeStatus[] = ["Awaiting Respondant", "Active", "Review"];
+
+const CHART_COLORS: Record<DisputeStatus, string> = {
+  "Awaiting Respondant": "red",
+  Active: "green",
+  Review: "orange",
+  Settled: "yellow",
+  Refused: "blue",
+  Withdrawn: "pink",
+  Transfer: "purple",
+  Appeal: "brown",
+  Other: "yellow",
+};
+
+const chartConfig = {} satisfies ChartConfig;
+
+export default function StatusPieChart({ data }: { data: Record<DisputeStatus, number> }) {
+  const totalCount = React.useMemo(() => {
+    return Object.values(data).reduce((acc, curr) => acc + curr, 0);
+  }, [data]);
+
+  const totalOpen = React.useMemo(() => {
+    const entries = Object.entries(data) as [DisputeStatus, number][];
+    return entries.reduce((acc, curr) => {
+      if (ACTIVE_STATUS.includes(curr[0])) {
+        return acc + curr[1];
+      }
+      return acc;
+    }, 0);
+  }, [data]);
+
+  const processed = DISPUTE_STATUS.map((status) => ({
+    status,
+    count: data[status],
+    fill: CHART_COLORS[status],
+  }));
+
+  return (
+    <Card className="mx-0">
+      <CardHeader>
+        <CardTitle>Overview</CardTitle>
+        <CardDescription>See the disputes currently in the system</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[250px]">
+          <PieChart>
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <Pie
+              data={processed}
+              dataKey="count"
+              nameKey="status"
+              innerRadius={60}
+              paddingAngle={2}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="dark:fill-white text-3xl font-bold"
+                        >
+                          {totalCount.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="dark:fill-white/50"
+                        >
+                          Disputes
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex flex-col items-stretch">
+        <p className="flex">
+          <strong className="grow">Open disputes:</strong>
+          <span>{((totalOpen / totalCount) * 100).toFixed(0)}%</span>
+        </p>
+        <p className="flex">
+          <strong className="grow">Resolved disputes:</strong>
+          <span>{((1 - totalOpen / totalCount) * 100).toFixed(0)}%</span>
+        </p>
+      </CardFooter>
+    </Card>
+  );
+}

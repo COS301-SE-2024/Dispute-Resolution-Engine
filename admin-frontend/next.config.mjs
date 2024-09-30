baeacd601fd20910470aa8b3a9432042322da3ec
Michael Horwitz <michael@horwitz.co.za>
2024-09-30 10:10:53 +0200

    fix: redirects for next

--- admin-frontend/next.config.mjs
+++ admin-frontend/next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/admin",
-  // assetPrefix: "/admin",
-  // rewrites() {
-  //   return [{ source: "/_next/:path*", destination: "/_next/:path*" }];
-  // },
+  assetPrefix: "/admin",
+  rewrites() {
+    return [{ source: "/admin/_next/:path*", destination: "/_next/:path*" }];
+  },
 };
 
 export default nextConfig;

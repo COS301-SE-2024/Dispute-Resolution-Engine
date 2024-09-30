a0d9182755c47ed7dcff7fda60417ef5db938a28
Caelan Hill <caelan.hill@gmail.com>
2024-09-30 13:04:12 +0200

    feat: added dispute proceedingslogger tests

--- api/auditLogger/mock.go
+++ api/auditLogger/mock.go
@@ -1,11 +1,25 @@
 package auditLogger
 
+import "api/models"
+
 type MockDisputeProceedingsLogger struct {
 	ThrowError bool
 	Error      error
 }
 
 func (m MockDisputeProceedingsLogger) LogDisputeProceedings(proceedingType string, eventData map[string]interface{}) error {
+	if m.ThrowError {
+		return m.Error
+	}
+	return nil
+}
+
+type MockDBDisputeProceedingsLogger struct {
+	ThrowError bool
+	Error      error
+}
+
+func (m MockDBDisputeProceedingsLogger) CreateLog(log models.EventLog) error {
 	if m.ThrowError {
 		return m.Error
 	}

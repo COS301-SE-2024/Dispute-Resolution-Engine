61abe8320e3b81f393c36416c36a52069f449946
Vincent <vafdaf11@gmail.com>
2024-10-02 14:57:45 +0200

    fix: Spacing issues on small screen sizes

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -13,13 +13,13 @@ export default function Signup() {
   return (
     <Providers>
       <div className="h-full overflow-y-auto">
-        <div className="mx-auto md:w-[40rem] p-4 grid grid-rows-[auto_1fr_auto] gap-5 pb-3 pt-5 h-full">
-          <header className="space-y-2">
+        <div className="mx-auto md:w-[40rem] grid grid-rows-[auto_1fr_auto] gap-5 px-5  h-full">
+          <header className="space-y-2 mt-5">
             <CardTitle>Signup</CardTitle>
             <CardDescription>Welcome to DRE! Tell us a little bit about yourself</CardDescription>
           </header>
           <SignupForm id={formId} />
-          <footer className="flex items-center flex-wrap gap-2">
+          <footer className="flex items-center flex-wrap gap-2 pb-4">
             <p className="grow">
               Already have an account?{" "}
               <Link href="/login" className="hover:underline">

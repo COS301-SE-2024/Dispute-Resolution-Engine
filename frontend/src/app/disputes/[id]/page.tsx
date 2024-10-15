e4d777a593c497916b12948cd4d83a31e66b3da2
Vincent <vafdaf11@gmail.com>
2024-10-15 21:03:38 +0200

    feat: Stub time estimate tooltip

--- frontend/src/app/disputes/[id]/page.tsx
+++ frontend/src/app/disputes/[id]/page.tsx
@@ -12,18 +12,13 @@ import {
   TicketCheckIcon,
   TriangleAlertIcon,
 } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { getAuthToken } from "@/lib/util/jwt";
 import ExpertRejectForm from "@/components/dispute/expert-reject-form";
 import DisputeDecisionForm from "@/components/dispute/decision-form";
 
 import { jwtDecode } from "jwt-decode";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 type Props = {
   params: { id: string };
@@ -51,6 +46,7 @@ export default async function DisputePage({ params }: Props) {
         date={data.case_date}
         status={data.status}
         state={workflow.definition.states[workflow.current_state]}
+        estimate="2 days"
       />
       <div className="overflow-y-auto p-5">
         <DisputeClientPage data={data} />
@@ -65,72 +61,84 @@ function DisputeHeader(props: {
   date: string;
   status: string;
   state: State;
+  estimate: string;
 }) {
   const user = (jwtDecode(getAuthToken()) as any).user.id;
   const role = (jwtDecode(getAuthToken()) as any).user.role;
 
   return (
-    <div className="border-b border-primary-500/30 grid grid-cols-[auto_1fr_auto] md:grid-cols-[auto_1fr_auto_auto] p-3 gap-3">
-      <Button asChild className="p-2 rounded-full" variant="ghost">
-        <Link href="/disputes">
-          <ChevronLeftIcon />
-        </Link>
-      </Button>
+    <TooltipProvider>
+      <div className="border-b border-primary-500/30 grid grid-cols-[auto_1fr_auto] md:grid-cols-[auto_1fr_auto_auto] p-3 gap-3">
+        <Button asChild className="p-2 rounded-full" variant="ghost">
+          <Link href="/disputes">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
 
-      <section className="mt-1">
-        <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-2xl text-wrap">
-          {props.title}
-        </h1>
-        <p>Filed: {props.date}</p>
-      </section>
+        <section className="mt-1">
+          <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-2xl text-wrap">
+            {props.title}
+          </h1>
 
-      <dl className="col-start-2 md:col-start-3 mt-1 grid grid-cols-[auto_1fr] gap-x-2">
-        <dt className="font-bold">Status:</dt>
-        <dd>{props.status}</dd>
-        <dt className="font-bold">State:</dt>
-        {/* <Tooltip>
-            <TooltipTrigger asChild> */}
-        <dd className="flex items-center gap-2">
-          {props.state.label}
-          <InfoIcon size="1rem" />
-        </dd>
-        {/* </TooltipTrigger>
-            <TooltipContent>{state.description}</TooltipContent>
-          </Tooltip> */}
-      </dl>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <p className="inline-flex items-center gap-2">
+                Filed: {props.date}
+                <InfoIcon size="1rem" />
+              </p>
+            </TooltipTrigger>
+            <TooltipContent>{`Estimated to take ${props.estimate}`}</TooltipContent>
+          </Tooltip>
+        </section>
 
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button className="p-2 rounded-full row-start-1 col-start-4" variant="ghost">
-            <EllipsisVerticalIcon />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="flex flex-col p-2">
-          <Button className="gap-2 p-2" variant="ghost" asChild>
-            <Link href={`/disputes/${props.id}/tickets`}>
-              <TicketCheckIcon />
-              <span>Dispute tickets</span>
-            </Link>
-          </Button>
-          {role == "expert" && (
-            <>
-              <DisputeDecisionForm disputeId={props.id} asChild>
-                <Button className="gap-2 p-2" variant="ghost">
-                  <BadgeCheckIcon />
-                  <span>Render decision</span>
-                </Button>
-              </DisputeDecisionForm>
-              <ExpertRejectForm expertId={user} disputeId={props.id} asChild>
-                <Button className="gap-2 p-2 text-red-500" variant="ghost">
-                  <TriangleAlertIcon />
-                  <span>Object to assignment</span>
-                </Button>
-              </ExpertRejectForm>
-            </>
-          )}
-        </PopoverContent>
-      </Popover>
-    </div>
+        <dl className="col-start-2 md:col-start-3 mt-1 grid grid-cols-[auto_1fr] gap-x-2">
+          <dt className="font-bold">Status:</dt>
+          <dd>{props.status}</dd>
+          <dt className="font-bold">State:</dt>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <dd className="flex items-center gap-2">
+                {props.state.label}
+                <InfoIcon size="1rem" />
+              </dd>
+            </TooltipTrigger>
+            <TooltipContent>{props.state.description}</TooltipContent>
+          </Tooltip>
+        </dl>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button className="p-2 rounded-full row-start-1 col-start-4" variant="ghost">
+              <EllipsisVerticalIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="flex flex-col p-2">
+            <Button className="gap-2 p-2" variant="ghost" asChild>
+              <Link href={`/disputes/${props.id}/tickets`}>
+                <TicketCheckIcon />
+                <span>Dispute tickets</span>
+              </Link>
+            </Button>
+            {role == "expert" && (
+              <>
+                <DisputeDecisionForm disputeId={props.id} asChild>
+                  <Button className="gap-2 p-2" variant="ghost">
+                    <BadgeCheckIcon />
+                    <span>Render decision</span>
+                  </Button>
+                </DisputeDecisionForm>
+                <ExpertRejectForm expertId={user} disputeId={props.id} asChild>
+                  <Button className="gap-2 p-2 text-red-500" variant="ghost">
+                    <TriangleAlertIcon />
+                    <span>Object to assignment</span>
+                  </Button>
+                </ExpertRejectForm>
+              </>
+            )}
+          </PopoverContent>
+        </Popover>
+      </div>
+    </TooltipProvider>
   );
 }
 
